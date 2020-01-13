76308ea79a409c311914d8a7fc82775754995c88
nikolai denissov <nikolai.denissov@aalto.fi>
2020-01-13 14:07:18 +0200

    making hints on missing plugins #9
    now opens "File | Settings | Plugins" window via hyperlink

--- src/main/java/fi/aalto/cs/intellij/actions/InstalledPluginsCheckerAction.java
+++ src/main/java/fi/aalto/cs/intellij/actions/InstalledPluginsCheckerAction.java
@@ -1,9 +1,13 @@
 package fi.aalto.cs.intellij.actions;
 
+import com.intellij.ide.actions.ShowSettingsUtilImpl;
 import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationAction;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
+import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.extensions.PluginId;
+import com.intellij.openapi.options.ShowSettingsUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
 import org.jetbrains.annotations.NotNull;
@@ -25,12 +29,21 @@ public class InstalledPluginsCheckerAction implements StartupActivity {
                     boolean isPluginOK = !isPluginInstalled(requiredPluginId)
                             || isDisabled(requiredPluginId.getIdString());
                     if (isPluginOK) {
-                        Notifications.Bus.notify(new Notification(
+                        Notification notification = new Notification(
                                 "A+",
                                 "A+",
-                                "Plugin " + requiredPluginName +
-                                        " must be installed and enabled for the A+ plugin to work properly.",
-                                NotificationType.WARNING));
+                                "<p>Plugin " + requiredPluginName +
+                                        " must be installed and enabled for the A+ plugin to work properly.</p>",
+                                NotificationType.WARNING);
+                        notification.addAction(new NotificationAction("Search for the " + requiredPluginName + " plugin.") {
+                            @Override
+                            public void actionPerformed(@NotNull AnActionEvent e, @NotNull Notification notification) {
+                                Project project = e.getProject();
+                                ShowSettingsUtil settingsUtil = ShowSettingsUtilImpl.getInstance();
+                                settingsUtil.showSettingsDialog(project, "Plugins");
+                            }
+                        });
+                        Notifications.Bus.notify(notification);
                     }
                 }
         );
