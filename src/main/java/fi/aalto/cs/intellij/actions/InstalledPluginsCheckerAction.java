ed7875aa782a208414011992a594d518bdb47eae
nikolai denissov <nikolai.denissov@aalto.fi>
2020-01-13 15:59:57 +0200

    making hints on missing plugins #9
    add a key-value type for storing required plugins data

--- src/main/java/fi/aalto/cs/intellij/actions/InstalledPluginsCheckerAction.java
+++ src/main/java/fi/aalto/cs/intellij/actions/InstalledPluginsCheckerAction.java
@@ -12,30 +12,32 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.intellij.ide.plugins.PluginManager.*;
 
 public class InstalledPluginsCheckerAction implements StartupActivity {
 
-    final static private String[] requiredPluginNames = {"org.intellij.scala"};
+    final static private Map<String, String> requiredPluginNames = new HashMap<>();
 
     @Override
     public void runActivity(@NotNull Project project) {
 
-        Arrays.stream(requiredPluginNames).forEach(
-                requiredPluginName -> {
-                    PluginId requiredPluginId = PluginId.getId(requiredPluginName);
+        requiredPluginNames.put("Scala", "org.intellij.scala");
+
+        requiredPluginNames.forEach((key, value) -> {
+                    PluginId requiredPluginId = PluginId.getId(value);
                     boolean isPluginOK = !isPluginInstalled(requiredPluginId)
                             || isDisabled(requiredPluginId.getIdString());
                     if (isPluginOK) {
                         Notification notification = new Notification(
                                 "A+",
                                 "A+",
-                                "<p>Plugin " + requiredPluginName +
+                                "<p>Plugin " + value +
                                         " must be installed and enabled for the A+ plugin to work properly.</p>",
                                 NotificationType.WARNING);
-                        notification.addAction(new NotificationAction("Search for the " + requiredPluginName + " plugin.") {
+                        notification.addAction(new NotificationAction("Search for the " + key + " plugin.") {
                             @Override
                             public void actionPerformed(@NotNull AnActionEvent e, @NotNull Notification notification) {
                                 Project project = e.getProject();
