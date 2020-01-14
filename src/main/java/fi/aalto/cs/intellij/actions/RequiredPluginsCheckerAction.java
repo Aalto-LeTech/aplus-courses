bc11c0d0b4befa70268ddd7a85d0419a6b4666f0
nikolai denissov <nikolai.denissov@aalto.fi>
2020-01-14 17:10:27 +0200

    making hints on missing plugins #9
    messy stuff!
    notifications are still there, but scala plugin now installs in the background

--- src/main/java/fi/aalto/cs/intellij/actions/RequiredPluginsCheckerAction.java
+++ src/main/java/fi/aalto/cs/intellij/actions/RequiredPluginsCheckerAction.java
@@ -3,34 +3,47 @@ package fi.aalto.cs.intellij.actions;
 import static com.intellij.ide.plugins.PluginManager.isDisabled;
 import static com.intellij.ide.plugins.PluginManager.isPluginInstalled;
 
-import com.intellij.ide.actions.ShowSettingsUtilImpl;
+import com.intellij.ide.plugins.IdeaPluginDescriptor;
 import com.intellij.ide.plugins.PluginManager;
 import com.intellij.ide.plugins.PluginManagerConfigurable;
+import com.intellij.ide.plugins.RepositoryHelper;
+import com.intellij.ide.plugins.newui.BgProgressIndicator;
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationAction;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.extensions.PluginId;
-import com.intellij.openapi.options.ShowSettingsUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
+import com.intellij.openapi.updateSettings.impl.PluginDownloader;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+//TODO REFACTOR THIS MESS
 public class RequiredPluginsCheckerAction implements StartupActivity {
 
   private static final Map<String, String> requiredPluginNames = new HashMap<>();
+  private IdeaPluginDescriptor descriptor;
 
   @Override
   public void runActivity(@NotNull Project project) {
+
+    try {
+      descriptor = getIdeaPluginDescriptor(PluginId.getId("org.intellij.scala"));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
     requiredPluginNames.put("Scala", "org.intellij.scala");
-    requiredPluginNames.forEach(this::process);
+    requiredPluginNames.forEach(this::checkPluginStatus);
   }
 
-  private void process(String key, String value) {
+  private void checkPluginStatus(String key, String value) {
     PluginId requiredPluginId = PluginId.getId(value);
     if (!isPluginInstalled(requiredPluginId)) {
       notifyAndOpenPluginSettingsWindow(key, value);
@@ -65,17 +78,35 @@ public class RequiredPluginsCheckerAction implements StartupActivity {
         "Plugin " + value
             + " must be installed and enabled for the A+ plugin to work properly.",
         NotificationType.WARNING);
+
     notification.addAction(new NotificationAction("Search for the " + key + " plugin.") {
 
       @Override
       public void actionPerformed(@NotNull AnActionEvent e, @NotNull Notification notification) {
-        Project project = e.getProject();
-        ShowSettingsUtil settingsUtil = ShowSettingsUtilImpl.getInstance();
-        PluginManagerConfigurable pluginManagerConfigurable = new PluginManagerConfigurable();
-        settingsUtil.editConfigurable(project, pluginManagerConfigurable);
+        try {
+          PluginDownloader pluginDownloader = PluginDownloader.createDownloader(descriptor);
+          pluginDownloader.prepareToInstall(new BgProgressIndicator());
+          pluginDownloader.install();
+          PluginManagerConfigurable.showRestartDialog("Plugins required for A+ course have been now installed");
+        } catch (IOException ex) {
+          ex.printStackTrace();
+        }
       }
     });
 
     Notifications.Bus.notify(notification);
+  }
+
+  @Nullable
+  private IdeaPluginDescriptor getIdeaPluginDescriptor(PluginId requiredPluginId)
+      throws IOException {
+    IdeaPluginDescriptor ideaPluginDescriptor;
+    List<IdeaPluginDescriptor> ideaPluginDescriptors = RepositoryHelper.loadPlugins(null);
+    ideaPluginDescriptor = ideaPluginDescriptors
+        .stream()
+        .filter(desc -> desc.getPluginId().equals(requiredPluginId))
+        .findFirst()
+        .orElse(null);
+    return ideaPluginDescriptor;
   }
 }
