87836f48d1b2e02b2ca33a2887f3060680a78f79
nikolai denissov <nikolai.denissov@aalto.fi>
2020-02-12 15:45:37 +0200

    required plugin gets enabled on action, but not persisted over restart #59
    add the script file itself

--- omytest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/apluslms/course-templates.git my_new_course \
- && cd my_new_course \
- && git submodule init \
- && git submodule update
---
+++ start_local_a+_env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+git clone https://github.com/apluslms/course-templates.git my_new_course &&
+cd my_new_course &&
+git submodule init &&
+git submodule update &&
+./docker-compile.sh &&
+head -97 docker-up.sh > docker-up-custom.sh &&
+chmod +x docker-up-custom.sh &&
+ls -al &&
+docker ps &&
+netstat -tulpn &&
+./docker-up-custom.sh &&
+docker ps &&
+netstat -tulpn &&
+curl --user root:root http://localhost:8000/api/v2/courses/
