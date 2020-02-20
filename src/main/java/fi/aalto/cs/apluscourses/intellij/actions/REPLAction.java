0e6293b471733f62746bc6b717489f63f7aa39b0
nikolai denissov <nikolai.denissov@aalto.fi>
2020-02-20 09:56:59 +0200

    investigate how Scala REPL thing can be done #65
    adds Scala plugin, but this fails with 'cyclic inheritance issue' when trying to extend Scala class

---
+++ src/main/java/fi/aalto/cs/apluscourses/intellij/actions/REPLAction.java
@@ -0,0 +1,8 @@
+package fi.aalto.cs.apluscourses.intellij.actions;
+
+import org.jetbrains.plugins.scala.console.actions.RunConsoleAction;
+
+public class REPLAction extends RunConsoleAction {
+
+
+}
