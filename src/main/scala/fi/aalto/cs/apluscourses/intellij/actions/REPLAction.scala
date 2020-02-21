573fbe21f738d14cd6a11f4228c8d3e255407aa8
nikolai denissov <nikolai.denissov@aalto.fi>
2020-02-21 10:45:11 +0200

    investigate how Scala REPL thing can be done #65
    adds a way to: get the data for the file, on which REPL has been triggered on and the list of (relevant) available modules

--- src/main/scala/fi/aalto/cs/apluscourses/intellij/actions/REPLAction.scala
+++ src/main/scala/fi/aalto/cs/apluscourses/intellij/actions/REPLAction.scala
@@ -1,12 +1,25 @@
 package fi.aalto.cs.apluscourses.intellij.actions
 
-import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.{AnActionEvent, CommonDataKeys}
+import com.intellij.openapi.module.{ModuleManager, ModuleUtilCore}
 import org.jetbrains.plugins.scala.console.actions.RunConsoleAction
 
-class REPLAction extends RunConsoleAction{
+class REPLAction extends RunConsoleAction {
 
   override def actionPerformed(e: AnActionEvent): Unit = {
     println("inside REPLAction: yeah, baby, yeah!")
+
+    val dataContext = e.getDataContext
+    val file = CommonDataKeys.PSI_FILE.getData(dataContext)
+    val module = ModuleUtilCore.findModuleForFile(file)
+
+    println("current module: " + module.getName + " for file: " + file)
+
+    val allModules = ModuleManager.getInstance(e.getProject).getModules.filter(_.getModuleTypeName == "JAVA_MODULE")
+
+    println("available modules:")
+    allModules.foreach(ev => println("  - " + ev.getName))
+
     super.actionPerformed(e)
   }
 }
