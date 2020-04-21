bbc3d72923bca32f66501e80eb62d0f65307a398
nikolai denissov <nikolai.denissov@aalto.fi>
2020-04-21 19:49:21 +0300

    mark modules as invalid if deps removed (O1L or else) #109
    submerges the test branch and fixes, adds tests

--- src/main/java/fi/aalto/cs/apluscourses/intellij/model/IntelliJModelFactory.java
+++ src/main/java/fi/aalto/cs/apluscourses/intellij/model/IntelliJModelFactory.java
@@ -9,14 +9,15 @@ import com.intellij.openapi.vfs.VirtualFileManager;
 import com.intellij.openapi.vfs.newvfs.BulkFileListener;
 import com.intellij.openapi.vfs.newvfs.events.VFileDeleteEvent;
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
+import fi.aalto.cs.apluscourses.model.ComponentLoadException;
 import fi.aalto.cs.apluscourses.model.Course;
 import fi.aalto.cs.apluscourses.model.Library;
 import fi.aalto.cs.apluscourses.model.ModelFactory;
 import fi.aalto.cs.apluscourses.model.Module;
+import fi.aalto.cs.apluscourses.utils.StateMonitor;
 import java.net.URL;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.Objects;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -36,10 +37,10 @@ public class IntelliJModelFactory implements ModelFactory {
 
   @Override
   public Course createCourse(@NotNull String name,
-                             @NotNull List<Module> modules,
-                             @NotNull List<Library> libraries,
-                             @NotNull Map<String, String> requiredPlugins,
-                             @NotNull Map<String, URL> resourceUrls) {
+      @NotNull List<Module> modules,
+      @NotNull List<Library> libraries,
+      @NotNull Map<String, String> requiredPlugins,
+      @NotNull Map<String, URL> resourceUrls) {
     IntelliJCourse course =
         new IntelliJCourse(name, modules, libraries, requiredPlugins, resourceUrls, project);
     // Add a module change listener with the created course instance to the project
@@ -47,8 +48,9 @@ public class IntelliJModelFactory implements ModelFactory {
     project.getMessageBus().connect().subscribe(ProjectTopics.MODULES, new ModuleListener() {
       @Override
       public void moduleRemoved(@NotNull Project project,
-                                @NotNull com.intellij.openapi.module.Module projectModule) {
-          course.onComponentRemove(course.getComponentIfExists(projectModule.getName()));
+          @NotNull com.intellij.openapi.module.Module projectModule) {
+        markDependentModulesInvalid(course, projectModule.getName());
+        course.onComponentRemove(course.getComponentIfExists(projectModule.getName()));
       }
     });
     project.getLibraryTable().addListener(new LibraryTable.Listener() {
@@ -87,17 +89,25 @@ public class IntelliJModelFactory implements ModelFactory {
         "Only common libraries like Scala SDK are currently supported.");
   }
 
-  private void markDependentModulesInvalid(@NotNull IntelliJCourse course,
+  /**
+   * This buddy here does all the nasty heavy lifting in order to check whether there are {@link
+   * Module} that depend on the removed {@link com.intellij.openapi.module.Module} and if finds,
+   * marks their {@link Module}s {@link StateMonitor} state.
+   *
+   * @param course            a {@link Course} which {@link Module}s to check
+   * @param removedModuleName a {@link String} name of the removed
+   * {@link com.intellij.openapi.module.Module}
+   */
+  public void markDependentModulesInvalid(@NotNull IntelliJCourse course,
       @NotNull String removedModuleName) {
-    Project project = course.getProject();
-    com.intellij.openapi.module.Module[] projectModules = ModuleManager.getInstance(project)
-        .getModules();
+    APlusProject project = course.getProject();
+    com.intellij.openapi.module.Module[] projectModules = project.getModuleManager().getModules();
 
     for (com.intellij.openapi.module.Module module : projectModules) {
-      Module courseModule = getCourseModule(course, module);
+      Module courseModule = (Module) course.getComponentIfExists(module.getName());
       if (courseModule != null) {
         List<String> courseModuleDependencies = getCourseModuleDependencies(courseModule);
-        if (courseModuleDependencies != null
+        if (courseModuleDependencies != null && !courseModuleDependencies.isEmpty()
             && courseModuleDependencies.contains(removedModuleName)) {
           courseModule.stateMonitor.set(StateMonitor.ERROR);
         }
@@ -105,25 +115,20 @@ public class IntelliJModelFactory implements ModelFactory {
     }
   }
 
+  /**
+   * A simple wrapper method on {@link Module#getDependencies()} to hide the thrown {@link
+   * ComponentLoadException}.
+   *
+   * @param courseModule a {@link Module} to get dependencies from
+   * @return a {@link List} of {@link String}s representing names of the dependencies.
+   */
   @Nullable
-  private Module getCourseModule(@NotNull IntelliJCourse course,
-      @NotNull com.intellij.openapi.module.Module module) {
-    Module courseModule = null;
-    try {
-      courseModule = course.getModule(module.getName());
-    } catch (NoSuchModuleException e) {
-      logger.error(e.getMessage(), e);
-    }
-    return courseModule;
-  }
-
-  @Nullable
-  private List<String> getCourseModuleDependencies(@NotNull Module courseModule) {
+  public List<String> getCourseModuleDependencies(@NotNull Module courseModule) {
     List<String> courseModuleDependencies = null;
     try {
       courseModuleDependencies = courseModule.getDependencies();
-    } catch (ModuleLoadException e) {
-      logger.error(e.getMessage(), e);
+    } catch (ComponentLoadException ex) {
+      logger.error(ex.getMessage(), ex);
     }
     return courseModuleDependencies;
   }
