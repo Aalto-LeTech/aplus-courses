1c9a4354772ce403f9f5767c28e94af38b8ad5df
nikolai denissov <nikolai.denissov@aalto.fi>
2020-05-15 14:48:58 +0300

    Make Actions "DumbAware" when possible #129
    removing the unnecessary IDs

--- src/main/java/fi/aalto/cs/apluscourses/intellij/actions/ActionGroups.java
+++ src/main/java/fi/aalto/cs/apluscourses/intellij/actions/ActionGroups.java
@@ -6,12 +6,6 @@ public class ActionGroups {
       "fi.aalto.cs.apluscourses.intellij.actions.ActionGroups.MODULE_ACTIONS";
   public static final String MENU_ACTIONS =
       "fi.aalto.cs.apluscourses.intellij.actions.ActionGroups.MENU_ACTIONS";
-  public static final String IMPORT_COURSE_IDE_SETTINGS =
-      "fi.aalto.cs.apluscourses.intellij.actions.ActionGroups.IMPORT_COURSE_IDE_SETTINGS";
-  public static final String IMPORT_COURSE_PROJECT_SETTINGS =
-      "fi.aalto.cs.apluscourses.intellij.actions.ActionGroups.IMPORT_COURSE_PROJECT_SETTINGS";
-  public static final String ABOUT_A_PLUS_PLUGIN =
-      "fi.aalto.cs.apluscourses.intellij.actions.ActionGroups.ABOUT_A_PLUS_PLUGIN";
 
   private ActionGroups() {
 
