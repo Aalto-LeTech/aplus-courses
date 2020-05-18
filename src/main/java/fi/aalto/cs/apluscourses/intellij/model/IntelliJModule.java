fbbdc1f44fe4063a317b43c626898a42364f59d2
nikke234 <nikolas.drosdek@aalto.fi>
2020-05-19 01:04:49 +0300

    Update module id from ZIP if id file is found

--- src/main/java/fi/aalto/cs/apluscourses/intellij/model/IntelliJModule.java
+++ src/main/java/fi/aalto/cs/apluscourses/intellij/model/IntelliJModule.java
@@ -13,12 +13,15 @@ import fi.aalto.cs.apluscourses.utils.DirAwareZipFile;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+
+import org.apache.commons.io.FileUtils;
 import org.jdom.JDOMException;
 import org.jetbrains.annotations.CalledWithWriteLock;
 import org.jetbrains.annotations.NotNull;
@@ -57,6 +60,10 @@ class IntelliJModule
     File tempZipFile = createTempFile();
     fetchZipTo(tempZipFile);
     extractZip(tempZipFile);
+    String newId = getIdFileContents();
+    if (newId != null) {
+      setVersionId(newId);
+    }
   }
 
   @Override
@@ -106,6 +113,22 @@ class IntelliJModule
     new DirAwareZipFile(file).extractDir(getName(), getFullPath().toString());
   }
 
+  /*
+   * This method looks for a special ID file in the module files root. If the file is found, the
+   * contents of the file are returned. If the file doesn't exist, or any IO error occurs, null is
+   * returned. Differentiating between a missing file and IO errors isn't important here, as we can
+   * always fall back to the ID from the course configuration file, and the ID file is optional.
+   * This method should only be called after extractZip has been called.
+   */
+  @Nullable
+  private String getIdFileContents() {
+    File idFile = getFullPath().resolve(".module_id").toFile();
+    try {
+      return FileUtils.readFileToString(idFile, StandardCharsets.UTF_8);
+    } catch (IOException ignored) {
+      return null;
+    }
+  }
 
   private void fetchZipTo(File file) throws IOException {
     CoursesClient.fetchZip(getUrl(), file);
