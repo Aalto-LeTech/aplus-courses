71b0dd31047636bd17fa72f5f15e1d6f7c8d50ee
nikke234 <nikolas.drosdek@aalto.fi>
2020-06-29 15:18:47 +0300

    Add SubmissionHistory

---
+++ src/main/java/fi/aalto/cs/apluscourses/model/SubmissionHistory.java
@@ -0,0 +1,45 @@
+package fi.aalto.cs.apluscourses.model;
+
+import fi.aalto.cs.apluscourses.intellij.services.PluginSettings;
+import fi.aalto.cs.apluscourses.utils.CoursesClient;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+public class SubmissionHistory {
+
+  private int numberOfSubmissions;
+
+  public SubmissionHistory(int numberOfSubmissions) {
+    this.numberOfSubmissions = numberOfSubmissions;
+  }
+
+  @NotNull
+  public static SubmissionHistory fromJsonObject(@NotNull JSONObject jsonObject) {
+    int count = jsonObject.getInt("count");
+    return new SubmissionHistory(count);
+  }
+
+  /**
+   * Get the submission history for the given exercise from the A+ API.
+   *
+   * @throws IOException If an IO error occurs (e.g. network error).
+   */
+  @NotNull
+  public static SubmissionHistory getSubmissionHistory(
+      long exerciseId, @NotNull APlusAuthentication authentication) throws IOException {
+    URL url = new URL(PluginSettings.A_PLUS_API_BASE_URL + "/exercises/" + exerciseId
+        + "/submissions/me/");
+    InputStream inputStream = CoursesClient.fetch(url, authentication::addToRequest);
+    JSONObject response = new JSONObject(new JSONTokener(inputStream));
+    return fromJsonObject(response);
+  }
+
+  public int getNumberOfSubmissions() {
+    return numberOfSubmissions;
+  }
+
+}
