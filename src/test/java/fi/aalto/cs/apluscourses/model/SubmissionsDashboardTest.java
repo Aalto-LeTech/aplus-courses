8f27fb30543ae3a9836742cf902bfb22cfcb6a53
nikolai denissov <nikolai.denissov@aalto.fi>
2020-07-08 15:49:31 +0300

    submission status report #208
    remove the weak!

--- src/test/java/fi/aalto/cs/apluscourses/model/SubmissionsDashboardTest.java
+++ src/test/java/fi/aalto/cs/apluscourses/model/SubmissionsDashboardTest.java
@@ -2,19 +2,10 @@ package fi.aalto.cs.apluscourses.model;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.IOException;
 import org.json.JSONObject;
 import org.junit.Test;
 
 public class SubmissionsDashboardTest {
-
-  @Test
-  public void testGetSubmissionsResults() throws IOException {
-    String token = "92384573df6ba9e45ac6297cc2b23a38592c90f7";
-    char[] charSetToken = token.toCharArray();
-    SubmissionsDashboard submissionsDashboard = SubmissionsDashboard
-        .getSubmissionsDashboard(163, new APlusAuthentication(charSetToken));
-  }
 
   @Test
   public void testFromJsonObject() {
