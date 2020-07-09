5820c99a3dbc8e33d37febfb34510119a5f979d4
nikolai denissov <nikolai.denissov@aalto.fi>
2020-07-09 13:34:12 +0300

    persistant storage of the credentials (token) #185
    adds the implementation with the JB's PasswordSafe

--- src/main/java/fi/aalto/cs/apluscourses/model/APlusAuthentication.java
+++ src/main/java/fi/aalto/cs/apluscourses/model/APlusAuthentication.java
@@ -1,27 +1,60 @@
 package fi.aalto.cs.apluscourses.model;
 
+import com.intellij.credentialStore.CredentialAttributes;
+import com.intellij.credentialStore.CredentialAttributesKt;
+import com.intellij.credentialStore.Credentials;
+import com.intellij.ide.passwordSafe.PasswordSafe;
 import fi.aalto.cs.apluscourses.utils.CoursesClient;
-import java.util.Arrays;
 import org.apache.http.HttpRequest;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class APlusAuthentication implements CoursesClient.Authentication {
+
+  public static final String A_COURSES_PLUGIN = "A+ Courses Plugin";
+  public static final String A_API = "A+ API";
+
   @NotNull
-  private final char[] token;
+  private final CredentialAttributes credentialAttributes;
+
+  public APlusAuthentication() {
+    this.credentialAttributes = createCredentialAttributes();
+  }
 
   /**
    * Initializes an instance with the given token. Note, that the given token array is not cleared
    * or overwritten.
    */
   public APlusAuthentication(@NotNull char[] token) {
-    this.token = token.clone();
+    this();
+    setToken(token.clone());
+  }
+
+  public void setToken(@NotNull char[] token) {
+    Credentials credentials = new Credentials("A+_user", token.clone());
+    PasswordSafe.getInstance().set(credentialAttributes, credentials);
+  }
+
+  @Nullable
+  public char[] getToken() {
+    Credentials credentials = PasswordSafe.getInstance().get(credentialAttributes);
+    if (credentials != null) {
+      return credentials.getPassword().toCharArray(true);
+    }
+    return null;
+  }
+
+  public void deleteTokenFromStorage() {
+    PasswordSafe.getInstance().set(credentialAttributes, null);
   }
 
   public void addToRequest(@NotNull HttpRequest request) {
-    request.addHeader("Authorization", "Token " + new String(token));
+    request.addHeader("Authorization", "Token " + new String(getToken()));
   }
 
-  public void clear() {
-    Arrays.fill(token, '\0');
+  @NotNull
+  private CredentialAttributes createCredentialAttributes() {
+    return new CredentialAttributes(
+        CredentialAttributesKt.generateServiceName(A_COURSES_PLUGIN, A_API));
   }
 }
