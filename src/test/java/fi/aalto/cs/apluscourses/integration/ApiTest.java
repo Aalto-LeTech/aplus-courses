8d154ed58f8238f73e93ccd62a243551a8288aab
nikolai denissov <nikolai.denissov@aalto.fi>
2020-07-14 11:35:09 +0300

    submission status report #208
    fix the API test (ute existing to work with the "student" user)

--- src/test/java/fi/aalto/cs/apluscourses/integration/ApiTest.java
+++ src/test/java/fi/aalto/cs/apluscourses/integration/ApiTest.java
@@ -1,6 +1,9 @@
 package fi.aalto.cs.apluscourses.integration;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.hasItems;
 
 import fi.aalto.cs.apluscourses.model.SubmissionsDashboard;
@@ -18,18 +21,28 @@ public class ApiTest {
 
   @Test
   public void getSubmissionsResultsReturns() throws MalformedURLException {
+    final String firstExercise = "modules.exercises[0]";
+
     given()
         .auth()
         .preemptive()
-        .basic("root", "root")
+        .basic("zoralst1", "zoralst1")
         .when()
         .contentType(ContentType.JSON)
-        .get(SubmissionsDashboard.getSubmissionsDashboardApiUrl(163).toString())
+        .get(SubmissionsDashboard.getSubmissionsDashboardApiUrl(100).toString())
         .then()
         .assertThat()
         .statusCode(HttpStatus.SC_OK)
-        .body("members.id", hasItems(503, 504))
-        .body("members.username", hasItems("student5", "student6"));
+        .body("modules.id", hasItem(3))
+        .body(firstExercise + ".id", hasItem(300))
+        .body(firstExercise + ".max_points", hasItem(100))
+        .body(firstExercise + ".points_to_pass", hasItem(50))
+        .body(firstExercise + ".submission_count", hasItem(3))
+        .body(firstExercise + ".points", hasItem(60))
+        .body(firstExercise + ".passed", hasItem(true))
+        .body(firstExercise + ".submissions[0][0]", containsString("402"))
+        .body(firstExercise + ".submissions[0][1]", containsString("401"))
+        .body(firstExercise + ".submissions[0][2]", containsString("400"));
   }
 
   @Test
@@ -37,14 +50,15 @@ public class ApiTest {
     given()
         .auth()
         .preemptive()
-        .basic("root", "root")
+        .basic("zoralst1", "zoralst1")
         .when()
         .contentType(ContentType.JSON)
-        .get("http://localhost:8000/api/v2/courses/100/groups/200/")
+        .get("http://localhost:8000/api/v2/courses/100/mygroups/")
         .then()
         .assertThat()
         .statusCode(HttpStatus.SC_OK)
-        .body("members.id", hasItems(500, 501))
-        .body("members.username", hasItems("percash0", "zoralst1"));
+        .body("results[0].id", equalTo(200))
+        .body("results[0].members.id", hasItems(5, 6))
+        .body("results[0].members.full_name", hasItems("Perry Cash", "Zorita Alston"));
   }
 }
