480fc63b89ee825e61fc85608118f346821bce50
nikke234 <nikolas.drosdek@aalto.fi>
2020-07-27 13:51:02 +0300

    Disable GetSubmissionDashboardAction

--- src/main/java/fi/aalto/cs/apluscourses/intellij/actions/GetSubmissionsDashboardAction.java
+++ src/main/java/fi/aalto/cs/apluscourses/intellij/actions/GetSubmissionsDashboardAction.java
@@ -1,23 +1,12 @@
 package fi.aalto.cs.apluscourses.intellij.actions;
 
-import static java.util.Objects.requireNonNull;
-
 import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.DumbAwareAction;
-import com.intellij.openapi.project.Project;
-import fi.aalto.cs.apluscourses.intellij.notifications.NetworkErrorNotification;
 import fi.aalto.cs.apluscourses.intellij.notifications.Notifier;
 import fi.aalto.cs.apluscourses.intellij.services.MainViewModelProvider;
 import fi.aalto.cs.apluscourses.intellij.services.PluginSettings;
-import fi.aalto.cs.apluscourses.model.APlusAuthentication;
-import fi.aalto.cs.apluscourses.model.Course;
-import fi.aalto.cs.apluscourses.model.ExerciseDataSource;
-import fi.aalto.cs.apluscourses.model.SubmissionResultsList;
-import fi.aalto.cs.apluscourses.presentation.MainViewModel;
-import java.io.IOException;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public class GetSubmissionsDashboardAction extends DumbAwareAction {
 
@@ -44,47 +33,11 @@ public class GetSubmissionsDashboardAction extends DumbAwareAction {
 
   @Override
   public void actionPerformed(@NotNull AnActionEvent e) {
-    Project project = e.getProject();
-    MainViewModel mainViewModel = mainViewModelProvider.getMainViewModel(project);
-    ExerciseDataSource exerciseDataSource = mainViewModel.getExerciseDataSource();
-
-    if (exerciseDataSource == null) {
-      return;
-    }
-
-    APlusAuthentication authentication =
-        (APlusAuthentication) exerciseDataSource.getAuthentication();
-
-    Course course = requireNonNull(mainViewModel.getCourseViewModel().get()).getModel();
-
-    SubmissionResultsList submissionResultsList = tryGetSubmissionsDashboard(
-        Long.parseLong(course.getId()), authentication, requireNonNull(project));
-    course.setSubmissionsDashboard(requireNonNull(submissionResultsList));
+    /*
+    This action will eventually be a generic update action, which updates the main view model. For
+    now it is disabled, since the points list is fetched as a "by-product" of the main view model
+    update that occurs every 15 minutes, and this potentially causes a NPE.
+     */
   }
 
-  /**
-   * A wrapper method for a method that pulls the A+ API for course exercises' results.
-   *
-   * @param courseId       an id of the course results data to pull.
-   * @param authentication an {@link APlusAuthentication} object to use with API.
-   * @param project        a current {@link Project} to notify to.
-   * @return a fully-inflated {@link SubmissionResultsList} or null if the API call was
-   *        unsuccessful.
-   */
-  @Nullable
-  public SubmissionResultsList tryGetSubmissionsDashboard(long courseId,
-                                                          @NotNull
-                                                              APlusAuthentication authentication,
-                                                          @NotNull Project project) {
-    try {
-      return SubmissionResultsList.getSubmissionResultsList(courseId, authentication);
-    } catch (IOException e) {
-      notifyNetworkError(e, project);
-      return null;
-    }
-  }
-
-  private void notifyNetworkError(@NotNull IOException exception, @Nullable Project project) {
-    notifier.notify(new NetworkErrorNotification(exception), project);
-  }
 }
