1404e1e417bb0ffe5d165ff7b34c0e395d57101a
nikke234 <nikolas.drosdek@aalto.fi>
2020-08-04 14:33:38 +0300

    Add SubmissionResult#fromJsonObject

--- src/main/java/fi/aalto/cs/apluscourses/model/SubmissionResult.java
+++ src/main/java/fi/aalto/cs/apluscourses/model/SubmissionResult.java
@@ -1,6 +1,7 @@
 package fi.aalto.cs.apluscourses.model;
 
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONObject;
 
 public class SubmissionResult {
 
@@ -25,6 +26,28 @@ public class SubmissionResult {
     this.submissionId = submissionId;
     this.status = status;
     this.exerciseUrl = exerciseUrl;
+  }
+
+  /**
+   * Construct a {@link SubmissionResult} instance from the given JSON object. The JSON object must
+   * contain an integer for the "id" key, a JSON object for the "exercise" key, and optionally a
+   * string value for the "status" key.
+   */
+  @NotNull
+  public static SubmissionResult fromJsonObject(@NotNull JSONObject jsonObject) {
+    long id = jsonObject.getLong("id");
+
+    String exerciseUrl = jsonObject.getJSONObject("exercise").getString("html_url");
+
+    Status status = Status.UNKNOWN;
+    String statusString = jsonObject.optString("status");
+    if ("ready".equals(statusString)) {
+      status = Status.GRADED;
+    } else if ("unofficial".equals(statusString)) {
+      status = Status.UNOFFICIAL;
+    }
+
+    return new SubmissionResult(id, status, exerciseUrl);
   }
 
   public long getId() {
--- src/test/java/fi/aalto/cs/apluscourses/model/SubmissionResultTest.java
+++ src/test/java/fi/aalto/cs/apluscourses/model/SubmissionResultTest.java
@@ -2,6 +2,7 @@ package fi.aalto.cs.apluscourses.model;
 
 import static org.junit.Assert.assertEquals;
 
+import org.json.JSONObject;
 import org.junit.Test;
 
 public class SubmissionResultTest {
@@ -16,6 +17,23 @@ public class SubmissionResultTest {
         SubmissionResult.Status.GRADED, submissionResult.getStatus());
     assertEquals("The submission URL is correct", "http://example.com/submissions/123/",
         submissionResult.getUrl());
+  }
+
+  @Test
+  public void testFromJsonObject() {
+    JSONObject jsonObject = new JSONObject()
+        .put("id", 234)
+        .put("exercise", new JSONObject()
+            .put("html_url", "https://example.com/"))
+        .put("status", "ready");
+    SubmissionResult submissionResult = SubmissionResult.fromJsonObject(jsonObject);
+
+    assertEquals("The ID is the same as the one in the JSON object",
+        234L, submissionResult.getId());
+    assertEquals("The status is parsed correctly from the JSON object",
+        SubmissionResult.Status.GRADED, submissionResult.getStatus());
+    assertEquals("The exercise URL is taken correctly from the JSON object",
+        "https://example.com/submissions/234/", submissionResult.getUrl());
   }
 
 }
