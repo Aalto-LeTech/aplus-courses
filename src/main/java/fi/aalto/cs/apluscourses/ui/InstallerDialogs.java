bc8ae732bea44f7a0e4c55f69df9445650e60472
nikolai denissov <nikolai.denissov@aalto.fi>
2020-08-06 13:27:56 +0300

    proof read the English text messages of the plugin #186
    UI (other)

--- src/main/java/fi/aalto/cs/apluscourses/ui/InstallerDialogs.java
+++ src/main/java/fi/aalto/cs/apluscourses/ui/InstallerDialogs.java
@@ -5,10 +5,12 @@ import com.intellij.openapi.ui.Messages;
 import fi.aalto.cs.apluscourses.intellij.utils.EdtTask;
 import fi.aalto.cs.apluscourses.model.Component;
 import fi.aalto.cs.apluscourses.model.ComponentInstaller;
+import fi.aalto.cs.apluscourses.utils.PluginResourceBundle;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class InstallerDialogs implements ComponentInstaller.Dialogs {
+
   @Nullable
   private final Project project;
 
@@ -22,15 +24,17 @@ public class InstallerDialogs implements ComponentInstaller.Dialogs {
     return new EdtTask<Integer>() {
       @Override
       protected Integer execute() {
-        return Messages.showYesNoDialog(project, "CAUTION! Updating a module will overwrite your"
-                + " local changes in the module. Are you sure you want to update " + name  + "?",
-            name, Messages.getWarningIcon());
+        return Messages.showYesNoDialog(project,
+            PluginResourceBundle.getAndReplaceText("ui.installerDialogs.caution", name),
+            name,
+            Messages.getWarningIcon());
       }
     }.executeAndWait() == Messages.YES;
   }
 
   @FunctionalInterface
   public interface Factory {
+
     @NotNull
     public ComponentInstaller.Dialogs getDialogs(@Nullable Project project);
   }
