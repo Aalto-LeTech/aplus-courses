34e3004ed4606c04d8125b2d6980b411f0e6c2b5
Stanislav Franko <stanislav.franko@student.tuke.sk>
2020-10-23 14:14:29 +0200

    Implementing comments

--- src/main/java/fi/aalto/cs/apluscourses/presentation/exercise/SubmissionResultViewModel.java
+++ src/main/java/fi/aalto/cs/apluscourses/presentation/exercise/SubmissionResultViewModel.java
@@ -3,6 +3,7 @@ package fi.aalto.cs.apluscourses.presentation.exercise;
 import static fi.aalto.cs.apluscourses.utils.PluginResourceBundle.getText;
 
 import fi.aalto.cs.apluscourses.model.SubmissionResult;
+import fi.aalto.cs.apluscourses.presentation.base.BaseViewModel;
 import fi.aalto.cs.apluscourses.presentation.base.SelectableNodeViewModel;
 import org.jetbrains.annotations.NotNull;
 
@@ -24,10 +25,23 @@ public class SubmissionResultViewModel extends SelectableNodeViewModel<Submissio
     return getText("presentation.submissionResultViewModel.nameStart") + " " + submissionNumber;
   }
 
+
+  /**
+   * {@summary}Constructs text appearing in parenthesis.
+   *
+   * @return Returns status text for assignment.
+   *         Methods considers case assignment has not yet been graded
+   */
   @NotNull
   public String getStatusText() {
-    String status = getModel().getPoints() + "/" + getModel().getExercise().getMaxPoints() + " points";
-    return (getModel().getStatus() == SubmissionResult.Status.UNKNOWN) ? "In grading" : status;
+    SubmissionResult model = getModel();
+    return (model.getStatus() == SubmissionResult.Status.UNKNOWN)
+            ? getText("presentation.submissionResultViewModel.inGrading")
+            : getStatus(model);
+  }
+
+  private String getStatus(SubmissionResult model) {
+    return model.getPoints() + "/" + model.getExercise().getMaxPoints() + " points";
   }
 
   @Override
