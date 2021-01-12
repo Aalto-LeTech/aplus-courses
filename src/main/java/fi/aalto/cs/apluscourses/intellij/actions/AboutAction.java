3472114b56afd957406a5b7b874f613e5ef84a85
stellatsv <stellatsovou@gmail.com>
2021-01-12 22:14:07 +0200

    About dialog changes

--- src/main/java/fi/aalto/cs/apluscourses/intellij/actions/AboutAction.java
+++ src/main/java/fi/aalto/cs/apluscourses/intellij/actions/AboutAction.java
@@ -3,12 +3,17 @@ package fi.aalto.cs.apluscourses.intellij.actions;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.DumbAwareAction;
 import fi.aalto.cs.apluscourses.ui.AboutDialog;
+import java.io.IOException;
 import org.jetbrains.annotations.NotNull;
 
 public class AboutAction extends DumbAwareAction {
 
   @Override
   public void actionPerformed(@NotNull AnActionEvent e) {
-    AboutDialog.display();
+    try {
+      AboutDialog.display();
+    } catch (IOException ioException) {
+      ioException.printStackTrace();
+    }
   }
 }
--- src/main/java/fi/aalto/cs/apluscourses/ui/AboutDialog.java
+++ src/main/java/fi/aalto/cs/apluscourses/ui/AboutDialog.java
@@ -1,21 +1,26 @@
 package fi.aalto.cs.apluscourses.ui;
 
+import static fi.aalto.cs.apluscourses.utils.PluginResourceBundle.getAndReplaceText;
 import static fi.aalto.cs.apluscourses.utils.PluginResourceBundle.getText;
 
 import com.intellij.icons.AllIcons;
 import com.intellij.ide.BrowserUtil;
 import com.intellij.ui.components.JBLabel;
 import com.intellij.ui.components.labels.LinkLabel;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.SwingConstants;
+
+import javax.swing.*;
+
+import icons.PluginIcons;
+import java.io.IOException;
+import java.util.Properties;
+import java.awt.*;
 import org.jetbrains.annotations.NotNull;
 
 public class AboutDialog {
 
   private static final String A_COURSES_PLUGIN_PAGE = "https://plugins.jetbrains.com/plugin/13634-a-courses";
+  private static final String A_PLUS_PAGE = "https://plus.cs.aalto.fi/";
+  private static final String GITHUB_PAGE = "https://github.com/Aalto-LeTech/intellij-plugin";
 
   private AboutDialog() {
 
@@ -24,25 +29,44 @@ public class AboutDialog {
   /**
    * Displays a small pop up window containing basic information about the plugin.
    */
-  public static void display() {
+  public static void display() throws IOException {
+
+    final Icon icon = PluginIcons.A_PLUS_LOGO;
     JOptionPane.showMessageDialog(null,
         createAboutInnerContainer(),
         getText("ui.aboutDialog.title"),
-        JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.INFORMATION_MESSAGE, icon);
   }
 
   @NotNull
-  private static JPanel createAboutInnerContainer() {
+  private static JPanel createAboutInnerContainer() throws IOException {
     JPanel panel = new JPanel();
     panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-
-    JBLabel label = createAboutTextLabel();
+    JBLabel version = createVersionTextLabel();
+    JBLabel about = createAboutTextLabel();
+    JBLabel authors = createAuthorsTextLabel();
+    JBLabel attributes = createAttributesTextLabel();
     LinkLabel<Object> linkLabel = createPluginWebsiteLinkLabel();
-    panel.add(label);
+    LinkLabel<Object> linkLabelGithub = createGithubWebsiteLinkLabel();
+    LinkLabel<Object> linkLabelAPlus = createAPlusWebsiteLinkLabel();
+    panel.add(version);
+    panel.add(Box.createVerticalStrut(about.getFont().getSize()));
+    panel.add(about);
     panel.add(Box.createVerticalStrut(linkLabel.getFont().getSize()));
     panel.add(linkLabel);
-
+    panel.add(linkLabelGithub);
+    panel.add(linkLabelAPlus);
+    panel.add(Box.createVerticalStrut(authors.getFont().getSize()));
+    panel.add(authors);
+    panel.add(Box.createVerticalStrut(authors.getFont().getSize()));
+    panel.add(attributes);
     return panel;
+  }
+
+  private static JBLabel createAttributesTextLabel() {
+    JBLabel label = new JBLabel();
+    label.setText(getText("ui.aboutDialog.attributes"));
+    return label;
   }
 
   @NotNull
@@ -59,10 +83,54 @@ public class AboutDialog {
   }
 
   @NotNull
+  private static LinkLabel<Object> createAPlusWebsiteLinkLabel() {
+    LinkLabel<Object> linkLabel = new LinkLabel<>(
+            getText("ui.aboutDialog.APlusWebsite"),
+            AllIcons.Ide.External_link_arrow,
+            (first, second) ->
+                    BrowserUtil.browse(A_PLUS_PAGE));
+    linkLabel.setIconTextGap(0);
+    linkLabel.setHorizontalTextPosition(SwingConstants.LEFT);
+
+    return linkLabel;
+  }
+
+  @NotNull
+  private static LinkLabel<Object> createGithubWebsiteLinkLabel() {
+    LinkLabel<Object> linkLabel = new LinkLabel<>(
+            getText("ui.aboutDialog.GithubWebsite"),
+            AllIcons.Ide.External_link_arrow,
+            (first, second) ->
+                    BrowserUtil.browse(GITHUB_PAGE));
+    linkLabel.setIconTextGap(0);
+    linkLabel.setHorizontalTextPosition(SwingConstants.LEFT);
+
+    return linkLabel;
+  }
+  @NotNull
+  private static JBLabel createVersionTextLabel() throws IOException {
+    JBLabel label = new JBLabel();
+    Properties properties = new Properties();
+    properties.load(AboutDialog.class.getResourceAsStream("/build-info.properties"));
+    String version = properties.getProperty("version");
+    label.setText(getAndReplaceText("ui.aboutDialog.version", version));
+    return label;
+  }
+
+  @NotNull
   private static JBLabel createAboutTextLabel() {
     JBLabel label = new JBLabel();
+    label.setHorizontalTextPosition(SwingConstants.CENTER);
+    label.setAllowAutoWrapping(true);
     label.setText(getText("ui.aboutDialog.description"));
+    return label;
+  }
 
+  @NotNull
+  private static JBLabel createAuthorsTextLabel() {
+    JBLabel label = new JBLabel();
+    label.setHorizontalTextPosition(SwingConstants.CENTER);
+    label.setText(getText("ui.aboutDialog.authors"));
     return label;
   }
 }
