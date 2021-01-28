2ce1aefc4e53dfa4a4ec98734363869b7b62c552
Jaakko Närhi <jaakko.narhi@aalto.fi>
2021-01-28 13:26:31 +0200

    Change Desktop.browse to BrowserUtil.browse

--- src/main/java/fi/aalto/cs/apluscourses/model/UrlRenderer.java
+++ src/main/java/fi/aalto/cs/apluscourses/model/UrlRenderer.java
@@ -1,13 +1,13 @@
 package fi.aalto.cs.apluscourses.model;
 
-import java.awt.Desktop;
+import com.intellij.ide.BrowserUtil;
 import java.net.URI;
 import org.jetbrains.annotations.NotNull;
 
 public class UrlRenderer {
 
   public void show(@NotNull String url) throws Exception {
-    Desktop.getDesktop().browse(new URI(url));
+    BrowserUtil.browse(new URI(url));
   }
 
 }
