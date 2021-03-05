35fe054f4c1fd690ff2a1cf4340557bfeb63b07d
Jaakko Närhi <34682147+jaakkonarhi@users.noreply.github.com>
2021-03-05 19:41:37 +0200

    Authentication test (#559)
    * Auth test #499
    
    * Use the token env variable #499
    
    * More general method names #499
    
    * Test invalid token #499
    
    * Test invalid token #499
    
    * Change previous hasText
    
    * Edit build.yml
    
    * Try with more attempts #499
    
    * Does this work
    
    * Change wrong token test to empty token test #499
    
    * Maybe it will work now

--- src/e2e/kotlin/fi/aalto/cs/apluscourses/e2e/PlaceholderTest.kt
+++ src/e2e/kotlin/fi/aalto/cs/apluscourses/e2e/PlaceholderTest.kt
@@ -12,48 +12,71 @@ import org.junit.Test
 import java.time.Duration
 
 class PlaceholderTest {
-  init {
-    StepLoggerInitializer.init()
-  }
+    init {
+        StepLoggerInitializer.init()
+    }
 
-  @Test
-  fun mainTest() = uiTest {
-    CommonSteps(this).createProject()
-    CommonSteps(this).openAPlusProjectWindow()
-    step("Cancel") {
-      with(dialog("Select Course")) {
-        button("Cancel").click()
-      }
-    }
-    CommonSteps(this).openAPlusProjectWindow()
-    step("Select course") {
-      with(dialog("Select Course")) {
-        findText("O1").click()
-        button("OK").click()
-      }
-    }
-    step("Choose settings") {
-      CommonSteps(this).aPlusSettings(true)
-    }
-    step("Assertions") {
-      with(ideFrame()) {
-        with(projectViewTree()) {
-          waitFor(
-            Duration.ofSeconds(60),
-            Duration.ofSeconds(1),
-            "O1Library not found in project view tree"
-          ) { hasText("O1Library") }
+    @Test
+    fun mainTest() = uiTest {
+        //step 2
+        CommonSteps(this).createProject()
+        //step 4
+        CommonSteps(this).openAPlusProjectWindow()
+        step("Cancel") {
+            with(dialog("Select Course")) {
+                button("Cancel").click()
+            }
         }
-        aPlusStripeButton().click()
-        with(modules()) {
-          waitFor(
-            Duration.ofSeconds(60),
-            Duration.ofSeconds(1),
-            "O1Library not found in modules list"
-          ) { hasText("O1Library") }
-          assertTrue("A module is installed", hasText("  [Installed]"))
+        CommonSteps(this).openAPlusProjectWindow()
+        step("Select course") {
+            with(dialog("Select Course")) {
+                findText("O1").click()
+                button("OK").click()
+            }
         }
-      }
+        step("Choose settings") {
+            CommonSteps(this).aPlusSettings(true)
+        }
+        step("Assertions") {
+            with(ideFrame()) {
+                with(projectViewTree()) {
+                    waitFor(
+                        Duration.ofSeconds(60),
+                        Duration.ofSeconds(1),
+                        "O1Library not found in project view tree"
+                    ) { hasText("O1Library") }
+                }
+                aPlusSideBarButton().click()
+                with(modules()) {
+                    waitFor(
+                        Duration.ofSeconds(60),
+                        Duration.ofSeconds(1),
+                        "O1Library not found in modules list"
+                    ) { hasText("O1Library") }
+                    assertTrue("A module is installed",
+                        hasText { textData -> textData.text.contains("[Installed]") })
+                }
+            }
+        }
+        //step 8
+        step("Authenticate with empty token") {
+            CommonSteps(this).setAPlusToken("")
+            with(dialog("A+ Token")) {
+                assertTrue("Token dialog still showing after clicking OK with empty token", isShowing)
+                button("Cancel").click()
+            }
+        }
+        step("Authenticate with right token") {
+            CommonSteps(this).setAPlusToken(System.getenv("APLUS_TEST_TOKEN"))
+            with(ideFrame()) {
+                with(assignments()) {
+                    waitFor(
+                        Duration.ofSeconds(60),
+                        Duration.ofSeconds(1),
+                        "Week 1 wasn't found in the assignments tree"
+                    ) { hasText("Week 1") }
+                }
+            }
+        }
     }
-  }
 }
