1d0c78f3bd2506b9d924432db5040b9dd39d3144
Jaakko Närhi <jaakko.narhi@aalto.fi>
2021-03-24 10:30:33 +0200

    try this again

--- src/e2e/kotlin/fi/aalto/cs/apluscourses/e2e/steps/CommonSteps.kt
+++ src/e2e/kotlin/fi/aalto/cs/apluscourses/e2e/steps/CommonSteps.kt
@@ -9,6 +9,8 @@ import fi.aalto.cs.apluscourses.e2e.fixtures.dialog
 import fi.aalto.cs.apluscourses.e2e.fixtures.heavyWeightWindow
 import fi.aalto.cs.apluscourses.e2e.fixtures.ideFrame
 import fi.aalto.cs.apluscourses.e2e.fixtures.welcomeFrame
+import java.awt.event.KeyEvent.VK_A
+import java.awt.event.KeyEvent.VK_META
 import java.awt.event.KeyEvent.VK_SHIFT
 import java.time.Duration
 
@@ -84,7 +86,7 @@ class CommonSteps(val remoteRobot: RemoteRobot) {
             with(ideFrame()) {
                 when {
                     isMac() -> keyboard {
-                        double(VK_SHIFT)
+                        hotKey(VK_SHIFT, VK_META, VK_A)
                         enterText(item)
                         enter()
                     }
