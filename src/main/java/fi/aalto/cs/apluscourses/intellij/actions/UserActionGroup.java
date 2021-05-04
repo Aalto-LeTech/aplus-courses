d3612429e9dcaa5b8fa0bcc48c2005b8391fd7d5
Jaakko Närhi <jaakko.narhi@aalto.fi>
2021-05-04 12:26:02 +0300

    Hide ugly triangle #519

--- src/main/java/fi/aalto/cs/apluscourses/intellij/actions/UserActionGroup.java
+++ src/main/java/fi/aalto/cs/apluscourses/intellij/actions/UserActionGroup.java
@@ -2,9 +2,9 @@ package fi.aalto.cs.apluscourses.intellij.actions;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.openapi.actionSystem.impl.ActionButton;
 import com.intellij.openapi.project.DumbAware;
 import fi.aalto.cs.apluscourses.intellij.services.CourseProjectProvider;
-import fi.aalto.cs.apluscourses.intellij.services.MainViewModelProvider;
 import fi.aalto.cs.apluscourses.intellij.services.PluginSettings;
 import icons.PluginIcons;
 import org.jetbrains.annotations.NotNull;
@@ -22,9 +22,10 @@ public class UserActionGroup extends DefaultActionGroup implements DumbAware {
 
   @Override
   public void update(@NotNull AnActionEvent e) {
+    e.getPresentation().putClientProperty(ActionButton.HIDE_DROPDOWN_ICON, Boolean.TRUE);
     var project = courseProjectProvider.getCourseProject(e.getProject());
     if (project != null) {
-      var userName = courseProjectProvider.getCourseProject(e.getProject()).getUserName();
+      var userName = project.getUserName();
       var loggedIn = !userName.equals("");
       var icon = loggedIn ? PluginIcons.A_PLUS_USER_LOGGED_IN : PluginIcons.A_PLUS_USER;
       var text = loggedIn ? "Logged in as " + userName : "Not Logged In";
