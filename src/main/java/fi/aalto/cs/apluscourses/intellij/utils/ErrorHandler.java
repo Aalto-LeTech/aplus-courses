efcc8d03eabd43427b5ae307ccfecb63833997ab
Taikelenn <taikelenn@kasumata.ee>
2021-05-24 13:24:42 +0300

    Serialized error data into a JSON
    Related to #359

--- src/main/java/fi/aalto/cs/apluscourses/intellij/utils/ErrorHandler.java
+++ src/main/java/fi/aalto/cs/apluscourses/intellij/utils/ErrorHandler.java
@@ -1,6 +1,7 @@
 package fi.aalto.cs.apluscourses.intellij.utils;
 
 import com.intellij.ide.DataManager;
+import com.intellij.idea.IdeaLogger;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.application.ApplicationManager;
@@ -13,12 +14,27 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.util.Consumer;
 import java.awt.Component;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.json.JSONObject;
 
 public class ErrorHandler extends ErrorReportSubmitter {
+  private JSONObject serializeErrorData(@Nullable String errorInfo,
+                                        @Nullable String lastActionId,
+                                        @NotNull List<String> stackTraces) {
+    return new JSONObject()
+        .put("errorInfo", errorInfo)
+        .put("lastAction", lastActionId)
+        .put("stackTraces", stackTraces);
+  }
+
   @Override
-  public @Nullable String getPrivacyNoticeText() {
+  public @NotNull String getPrivacyNoticeText() {
     return "By submitting a report, you agree to the <a href=\"\">privacy policy</a>.";
   }
 
@@ -38,6 +54,13 @@ public class ErrorHandler extends ErrorReportSubmitter {
     new Task.Backgroundable(project, "Sending error report") {
       @Override
       public void run(@NotNull ProgressIndicator indicator) {
+        var serializedData = serializeErrorData(additionalInfo, IdeaLogger.ourLastActionId,
+            Arrays.stream(events).map(IdeaLoggingEvent::getThrowableText)
+                .collect(Collectors.toList()));
+
+        // TODO: send the JSON object to a remote server
+        System.err.println(serializedData.toString().length());
+
         ApplicationManager.getApplication().invokeLater(() -> {
           Messages.showInfoMessage(parentComponent, "Thank you for submitting the report.", "Error Report");
           consumer.consume(new SubmittedReportInfo(SubmittedReportInfo.SubmissionStatus.NEW_ISSUE));
