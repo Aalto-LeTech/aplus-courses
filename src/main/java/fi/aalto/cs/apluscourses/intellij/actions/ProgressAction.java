098c48f3449f2ec085a67e33082a1c583e3f72cb
Jaakko Närhi <jaakko.narhi@aalto.fi>
2021-05-28 12:07:26 +0300

    Change to JButtonAction #622

--- src/main/java/fi/aalto/cs/apluscourses/intellij/actions/ProgressAction.java
+++ src/main/java/fi/aalto/cs/apluscourses/intellij/actions/ProgressAction.java
@@ -3,23 +3,13 @@ package fi.aalto.cs.apluscourses.intellij.actions;
 import static fi.aalto.cs.apluscourses.utils.PluginResourceBundle.getAndReplaceText;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DefaultActionGroup;
-import com.intellij.openapi.actionSystem.Presentation;
-import com.intellij.openapi.actionSystem.ex.ComboBoxAction;
-import com.intellij.openapi.project.DumbAware;
-import com.intellij.ui.components.panels.NonOpaquePanel;
-import com.intellij.ui.scale.JBUIScale;
-import com.intellij.util.ui.JBUI;
-import com.intellij.util.ui.UIUtil;
+import com.intellij.util.ui.JButtonAction;
 import fi.aalto.cs.apluscourses.intellij.services.MainViewModelProvider;
 import fi.aalto.cs.apluscourses.intellij.services.PluginSettings;
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import javax.swing.JComponent;
+import icons.PluginIcons;
 import org.jetbrains.annotations.NotNull;
 
-public class ProgressAction extends ComboBoxAction implements DumbAware {
+public class ProgressAction extends JButtonAction {
 
   @NotNull
   private final MainViewModelProvider mainViewModelProvider;
@@ -29,6 +19,7 @@ public class ProgressAction extends ComboBoxAction implements DumbAware {
   }
 
   public ProgressAction(@NotNull MainViewModelProvider mainViewModelProvider) {
+    super("Progress", "Open IDE Activity dialog", PluginIcons.A_PLUS_INFO);
     this.mainViewModelProvider = mainViewModelProvider;
   }
 
@@ -41,6 +32,7 @@ public class ProgressAction extends ComboBoxAction implements DumbAware {
   public void update(@NotNull AnActionEvent e) {
     if (e.getProject() == null) {
       e.getPresentation().setVisible(false);
+      updateButtonFromPresentation(e);
       return;
     }
     var tutorialViewModel =
@@ -51,43 +43,7 @@ public class ProgressAction extends ComboBoxAction implements DumbAware {
       e.getPresentation().setText(getAndReplaceText("presentation.navbar.progress",
               tutorialViewModel.getCurrentTaskIndex() - 1, tutorialViewModel.getTasksAmount()));
     }
-  }
-
-  @NotNull
-  @Override
-  public JComponent createCustomComponent(@NotNull final Presentation presentation,
-                                          @NotNull String place) {
-    var button = new ComboBoxAction.ComboBoxButton(presentation) {
-      @Override
-      public Dimension getPreferredSize() {
-        Dimension d = super.getPreferredSize();
-        d.width = Math.max(d.width, JBUIScale.scale(75));
-        return d;
-      }
-
-
-      @Override
-      protected void fireActionPerformed(ActionEvent event) {
-        //performWhenButton(this, ActionPlaces.UNKNOWN);
-      }
-
-      @Override
-      protected boolean isArrowVisible(@NotNull Presentation presentation) {
-        return false;
-      }
-    };
-    var panel = new NonOpaquePanel(new BorderLayout());
-    var border = UIUtil.isUnderDefaultMacTheme()
-            ? JBUI.Borders.empty(0, 2) : JBUI.Borders.empty(0, 5, 0, 4);
-
-    panel.setBorder(border);
-    panel.add(button);
-    return panel;
-  }
-
-  @Override
-  protected @NotNull DefaultActionGroup createPopupActionGroup(JComponent button) {
-    return new DefaultActionGroup();
+    updateButtonFromPresentation(e);
   }
 
 }
