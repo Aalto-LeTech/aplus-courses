e687b62b6fd9a6d857a847d14537ad29446171f8
Taikelenn <taikelenn@kasumata.ee>
2021-06-01 12:24:28 +0300

    Balloon popups now have adjustable transparency

---
+++ src/main/java/fi/aalto/cs/apluscourses/ui/ideactivities/BalloonLabel.java
@@ -0,0 +1,31 @@
+package fi.aalto.cs.apluscourses.ui.ideactivities;
+
+import java.awt.AlphaComposite;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JLabel;
+
+public class BalloonLabel extends JLabel {
+  private float transparencyCoefficient;
+
+  public BalloonLabel(String text) {
+    super(text);
+    transparencyCoefficient = 1.0f;
+
+    setOpaque(false);
+  }
+
+  public void setTransparencyCoefficient(float coefficient) {
+    transparencyCoefficient = coefficient;
+  }
+
+  @Override
+  protected void paintComponent(Graphics g) {
+    Graphics2D g2 = (Graphics2D) g.create();
+    g2.setComposite(AlphaComposite.SrcOver.derive(transparencyCoefficient));
+
+    // let Swing do the text drawing using the customized Graphics object
+    super.paintComponent(g2);
+    g2.dispose();
+  }
+}
--- src/main/java/fi/aalto/cs/apluscourses/ui/ideactivities/BalloonPopup.java
+++ src/main/java/fi/aalto/cs/apluscourses/ui/ideactivities/BalloonPopup.java
@@ -2,6 +2,7 @@ package fi.aalto.cs.apluscourses.ui.ideactivities;
 
 import com.intellij.util.ui.JBUI;
 
+import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
@@ -10,16 +11,28 @@ import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import javax.swing.BoxLayout;
 import javax.swing.Icon;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
+
+import fi.aalto.cs.apluscourses.ui.GuiObject;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class BalloonPopup extends JPanel {
   private final @NotNull Component anchorComponent;
+
+  @GuiObject
+  private final JPanel titlePanel;
+
+  @GuiObject
+  private final BalloonLabel titleLabel;
+
+  @GuiObject
+  private final BalloonLabel messageLabel;
+
+  private float transparencyCoefficient;
 
   /**
    * Creates a popup with the given text. The popup is permanently attached to the specified
@@ -37,20 +50,21 @@ public class BalloonPopup extends JPanel {
     // introduce a limit to the popup's width (so it doesn't take the entire screen width)
     setMaximumSize(new Dimension(500, 0));
 
-    var titleText = new JLabel("<html><h1>" + title + "</h1></html>");
-    titleText.setIcon(icon);
+    titleLabel = new BalloonLabel("<html><h1>" + title + "</h1></html>");
+    titleLabel.setIcon(icon);
 
-    var titlePanel = new JPanel();
+    titlePanel = new JPanel();
     titlePanel.setOpaque(false);
     titlePanel.setLayout(new BoxLayout(titlePanel, BoxLayout.X_AXIS));
     titlePanel.setAlignmentX(LEFT_ALIGNMENT);
-    titlePanel.add(titleText);
+    titlePanel.add(titleLabel);
     add(titlePanel);
 
-    var messageText = new JLabel("<html>" + message + "</html>");
-    messageText.setAlignmentX(LEFT_ALIGNMENT);
-    add(messageText);
+    messageLabel = new BalloonLabel("<html>" + message + "</html>");
+    messageLabel.setAlignmentX(LEFT_ALIGNMENT);
+    add(messageLabel);
 
+    setTransparencyCoefficient(0.3f);
     recalculateBounds();
   }
 
@@ -60,13 +74,27 @@ public class BalloonPopup extends JPanel {
   }
 
   @Override
-  protected void paintComponent(Graphics graphics) {
-    super.paintComponent(graphics);
+  protected void paintComponent(Graphics g) {
+    super.paintComponent(g);
 
-    Graphics g = graphics.create();
-    g.setColor(getBackground());
-    Rectangle drawBounds = g.getClipBounds();
-    g.fillRect(drawBounds.x, drawBounds.y, drawBounds.width, drawBounds.height);
+    Graphics g2 = g.create();
+
+    var bgColor = getBackground();
+    var newColor = new Color(
+        bgColor.getRed(), bgColor.getGreen(), bgColor.getBlue(),
+        (int) (transparencyCoefficient * 255));
+    
+    g2.setColor(newColor);
+    Rectangle drawBounds = g2.getClipBounds();
+    g2.fillRect(drawBounds.x, drawBounds.y, drawBounds.width, drawBounds.height);
+
+    g2.dispose();
+  }
+
+  public void setTransparencyCoefficient(float coefficient) {
+    transparencyCoefficient = coefficient;
+    titleLabel.setTransparencyCoefficient(coefficient);
+    messageLabel.setTransparencyCoefficient(coefficient);
   }
 
   /**
