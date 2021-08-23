faa3e8805a5ab912209735bffce088a709a85bf4
Jaakko Närhi <jaakko.narhi@aalto.fi>
2021-08-23 18:44:22 +0300

    Add double-clicking to course selection view

--- src/main/java/fi/aalto/cs/apluscourses/presentation/CourseSelectionViewModel.java
+++ src/main/java/fi/aalto/cs/apluscourses/presentation/CourseSelectionViewModel.java
@@ -6,20 +6,29 @@ import fi.aalto.cs.apluscourses.utils.observable.ValidationError;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
+import java.util.Optional;
+import org.jetbrains.annotations.NotNull;
 
 public class CourseSelectionViewModel {
 
-  private List<CourseItemViewModel> courses;
+  @NotNull
+  public final ObservableProperty<CourseItemViewModel[]> courses
+      = new ObservableReadWriteProperty<>(new CourseItemViewModel[0]);
+
+  public final ObservableProperty<CourseItemViewModel> selectedCourse
+      = new ObservableReadWriteProperty<>(null);
 
   public final ObservableProperty<String> selectedCourseUrl
       = new ObservableReadWriteProperty<>("", CourseSelectionViewModel::validateCourseUrl);
 
+  /**
+   * A constructor.
+   */
   public CourseSelectionViewModel(List<CourseItemViewModel> courses) {
-    this.courses = courses;
-  }
-
-  public CourseItemViewModel[] getCourses() {
-    return courses.toArray(new CourseItemViewModel[0]);
+    this.courses.set(courses.toArray(CourseItemViewModel[]::new));
+    selectedCourse.addValueObserver(this, (self, course) ->
+        selectedCourseUrl.set(Optional.ofNullable(course)
+            .map(CourseItemViewModel::getUrl).orElse("")));
   }
 
   private static ValidationError validateCourseUrl(String courseUrl) {
--- src/main/java/fi/aalto/cs/apluscourses/ui/base/SingleSelectionList.java
+++ src/main/java/fi/aalto/cs/apluscourses/ui/base/SingleSelectionList.java
@@ -3,9 +3,12 @@ package fi.aalto.cs.apluscourses.ui.base;
 import com.intellij.ui.components.JBList;
 import fi.aalto.cs.apluscourses.ui.utils.Bindable;
 import fi.aalto.cs.apluscourses.ui.utils.TwoWayBindable;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
 import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import org.jetbrains.annotations.NotNull;
 
 public class SingleSelectionList<T> extends JBList<T> {
   public final transient TwoWayBindable<SingleSelectionList<T>, T> selectionBindable =
@@ -14,16 +17,35 @@ public class SingleSelectionList<T> extends JBList<T> {
   public final transient Bindable<SingleSelectionList<T>, T[]> listDataBindable =
       new Bindable<>(this, SingleSelectionList::setListData);
 
+  private final transient Runnable submitRunnable;
+
   /**
    * Constructs a new SingleSelectionListener.
    */
-  public SingleSelectionList() {
+  public SingleSelectionList(@NotNull Runnable submitRunnable) {
+    this.submitRunnable = submitRunnable;
     setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
     addListSelectionListener(new SelectionListener());
   }
 
   public void setSelectedValue(Object anObject) {
     setSelectedValue(anObject, true);
+  }
+
+  @Override
+  protected void processMouseEvent(MouseEvent e) {
+    super.processMouseEvent(e);
+    if (e.getClickCount() == 2) {
+      submitRunnable.run();
+    }
+  }
+
+  @Override
+  protected void processKeyEvent(KeyEvent e) {
+    super.processKeyEvent(e);
+    if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+      submitRunnable.run();
+    }
   }
 
   private class SelectionListener implements ListSelectionListener {
