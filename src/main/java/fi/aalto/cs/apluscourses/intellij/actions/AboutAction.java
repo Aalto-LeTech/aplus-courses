69da81ebaaecd13ea92641bbd6fc25fa24abfe7e
Taikelenn <taikelenn@kasumata.ee>
2021-11-08 15:35:33 +0200

    Removed testing code

--- src/main/java/fi/aalto/cs/apluscourses/intellij/actions/AboutAction.java
+++ src/main/java/fi/aalto/cs/apluscourses/intellij/actions/AboutAction.java
@@ -1,6 +1,5 @@
 package fi.aalto.cs.apluscourses.intellij.actions;
 
-import com.intellij.diagnostic.VMOptions;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.DumbAwareAction;
 import fi.aalto.cs.apluscourses.ui.AboutDialog;
@@ -10,10 +9,6 @@ public class AboutAction extends DumbAwareAction {
 
   @Override
   public void actionPerformed(@NotNull AnActionEvent e) {
-    if (VMOptions.canWriteOptions()) {
-      VMOptions.writeOption("console.encoding", "=", "UTF-8");
-    }
-
     AboutDialog.display();
   }
 }
