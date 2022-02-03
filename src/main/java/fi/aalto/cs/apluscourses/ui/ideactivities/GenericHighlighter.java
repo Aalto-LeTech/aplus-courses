6cbaca20cb01fd49eb2520b76fcff82b1aa9092f
Taikelenn <taikelenn@kasumata.ee>
2022-02-03 13:09:31 +0200

    Fixed mixing up two classes

--- src/main/java/fi/aalto/cs/apluscourses/ui/ideactivities/GenericHighlighter.java
+++ src/main/java/fi/aalto/cs/apluscourses/ui/ideactivities/GenericHighlighter.java
@@ -1,10 +1,8 @@
 package fi.aalto.cs.apluscourses.ui.ideactivities;
 
-import com.intellij.util.ui.JBValue;
 import java.awt.Component;
 import java.awt.Rectangle;
 import java.awt.geom.RectangularShape;
-import java.awt.geom.RoundRectangle2D;
 import java.util.Collections;
 import java.util.List;
 import org.jetbrains.annotations.NotNull;
@@ -20,15 +18,9 @@ public class GenericHighlighter {
     return component;
   }
 
-  /**
-   * Gets the area as RoundRectangles2D, with the arc length corresponding
-   * to the current theme's default arc length.
-   */
   public List<RectangularShape> getArea() {
-    final float arc = new JBValue.UIInteger("Button.arc", 6).getFloat();
     return Collections.singletonList(
-        new RoundRectangle2D.Float(
-            0, 0, getComponent().getWidth(), getComponent().getHeight(), arc, arc));
+        new Rectangle(0, 0, getComponent().getWidth(), getComponent().getHeight()));
   }
 
   public GenericHighlighter(@NotNull Component component) {
--- src/main/java/fi/aalto/cs/apluscourses/ui/ideactivities/RoundedHighlighter.java
+++ src/main/java/fi/aalto/cs/apluscourses/ui/ideactivities/RoundedHighlighter.java
@@ -1,14 +1,21 @@
 package fi.aalto.cs.apluscourses.ui.ideactivities;
 
+import com.intellij.util.ui.JBValue;
 import java.awt.Component;
 import java.awt.geom.RectangularShape;
+import java.awt.geom.RoundRectangle2D;
+import java.util.Collections;
 import java.util.List;
 import org.jetbrains.annotations.NotNull;
 
 public class RoundedHighlighter extends GenericHighlighter {
+
   @Override
   public List<RectangularShape> getArea() {
-    return super.getArea();
+    final float arc = new JBValue.UIInteger("Button.arc", 6).getFloat();
+    return Collections.singletonList(
+        new RoundRectangle2D.Float(
+            0, 0, getComponent().getWidth(), getComponent().getHeight(), arc, arc));
   }
 
   public RoundedHighlighter(@NotNull Component component) {
