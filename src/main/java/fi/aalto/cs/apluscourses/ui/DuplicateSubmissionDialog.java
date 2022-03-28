708118ab72946072a2bb60de92ec5e82f371fd62
Taikelenn <taikelenn@kasumata.ee>
2022-03-28 16:30:21 +0300

    Externalize strings, update tests

--- src/main/java/fi/aalto/cs/apluscourses/ui/DuplicateSubmissionDialog.java
+++ src/main/java/fi/aalto/cs/apluscourses/ui/DuplicateSubmissionDialog.java
@@ -1,5 +1,7 @@
 package fi.aalto.cs.apluscourses.ui;
 
+import static fi.aalto.cs.apluscourses.utils.PluginResourceBundle.getText;
+
 import javax.swing.JOptionPane;
 
 public class DuplicateSubmissionDialog {
@@ -13,11 +15,11 @@ public class DuplicateSubmissionDialog {
    * @return True if the user wants to proceed with the submission.
    */
   public static boolean showDialog() {
-    final String[] options = { "Submit anyway", "Don't submit" };
+    final String[] options = { getText("ui.duplicateDialog.yesOption"), getText("ui.duplicateDialog.noOption") };
 
     return JOptionPane.showOptionDialog(null,
-        "In the past, you have already made the exact same submission for this exercise.\nDo you wish to submit anyway?",
-        "Duplicate submission detected",
+        getText("ui.duplicateDialog.title"),
+        getText("ui.duplicateDialog.content"),
         JOptionPane.YES_NO_OPTION,
         JOptionPane.QUESTION_MESSAGE,
         null,
