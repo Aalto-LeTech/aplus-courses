3d57f3f3631b68c24c7d7b3d832e26a6289ff66a
Taikelenn <taikelenn@kasumata.ee>
2022-04-20 11:54:05 +0200

    Added A+ project generator

---
+++ src/main/java/fi/aalto/cs/apluscourses/intellij/generator/APlusModuleBuilder.java
@@ -0,0 +1,20 @@
+package fi.aalto.cs.apluscourses.intellij.generator;
+
+import com.intellij.ide.util.projectWizard.ModuleBuilder;
+
+public class APlusModuleBuilder extends ModuleBuilder {
+    @Override
+    public APlusModuleType getModuleType() {
+        return APlusModuleType.getInstance();
+    }
+
+    @Override
+    public boolean canCreateModule() {
+        return false;
+    }
+
+    @Override
+    public boolean isOpenProjectSettingsAfter() {
+        return true;
+    }
+}
