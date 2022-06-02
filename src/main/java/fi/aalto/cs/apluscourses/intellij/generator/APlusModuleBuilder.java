fcb55595667038e778e903f04d9823c052a16901
Taikelenn <taikelenn@kasumata.ee>
2022-06-02 12:57:53 +0300

    Removed useless components from the project creation window

--- src/main/java/fi/aalto/cs/apluscourses/intellij/generator/APlusModuleBuilder.java
+++ src/main/java/fi/aalto/cs/apluscourses/intellij/generator/APlusModuleBuilder.java
@@ -1,6 +1,12 @@
 package fi.aalto.cs.apluscourses.intellij.generator;
 
 import com.intellij.ide.util.projectWizard.ModuleBuilder;
+import com.intellij.ide.util.projectWizard.ModuleWizardStep;
+import com.intellij.ide.util.projectWizard.WizardContext;
+import com.intellij.openapi.Disposable;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import org.jetbrains.annotations.Nullable;
 
 public class APlusModuleBuilder extends ModuleBuilder {
   @Override
@@ -16,5 +22,21 @@ public class APlusModuleBuilder extends ModuleBuilder {
   @Override
   public boolean isOpenProjectSettingsAfter() {
     return true;
+  }
+
+  @Override
+  public @Nullable ModuleWizardStep getCustomOptionsStep(WizardContext context, Disposable parentDisposable) {
+    return new ModuleWizardStep() {
+      @Override
+      public JComponent getComponent() {
+        // create an empty JPanel so that nothing is shown in the project creation window
+        return new JPanel();
+      }
+
+      @Override
+      public void updateDataModel() {
+        // don't do anything
+      }
+    };
   }
 }
