1830ecd34a27efdea452af42856aeaa9a4a0b643
Taikelenn <taikelenn@kasumata.ee>
2023-05-04 00:43:26 +0300

    Add test function for hiding entries, change string to regex

--- src/main/java/fi/aalto/cs/apluscourses/utils/CourseHiddenElements.java
+++ src/main/java/fi/aalto/cs/apluscourses/utils/CourseHiddenElements.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.json.JSONArray;
@@ -13,22 +14,22 @@ import org.json.JSONObject;
 
 public class CourseHiddenElements {
 
-  private final @NotNull List<Integer> hiddenIDs = new ArrayList<>();
+  private final @NotNull List<Long> hiddenIDs = new ArrayList<>();
 
-  private final @NotNull List<String> hiddenRegexes = new ArrayList<>();
+  private final @NotNull List<Pattern> hiddenRegexes = new ArrayList<>();
 
-  private final @NotNull Map<String, List<String>> hiddenLanguageSpecificRegexes = new HashMap<>();
+  private final @NotNull Map<String, List<Pattern>> hiddenLanguageSpecificRegexes = new HashMap<>();
 
   @NotNull
   public static CourseHiddenElements fromJsonObject(@NotNull JSONArray hiddenEntriesArray) {
-    List<Integer> hiddenIDs = new ArrayList<>();
-    List<String> hiddenRegexes = new ArrayList<>();
-    Map<String, List<String>> hiddenLanguageSpecificRegexes = new HashMap<>();
+    List<Long> hiddenIDs = new ArrayList<>();
+    List<Pattern> hiddenRegexes = new ArrayList<>();
+    Map<String, List<Pattern>> hiddenLanguageSpecificRegexes = new HashMap<>();
 
     for (int i = 0; i < hiddenEntriesArray.length(); ++i) {
       JSONObject entryInfo = hiddenEntriesArray.getJSONObject(i);
 
-      int hiddenID = entryInfo.optInt("byId", -1);
+      long hiddenID = entryInfo.optLong("byId", -1);
       if (hiddenID != -1) {
         // Hide by exercise/group ID
         hiddenIDs.add(hiddenID);
@@ -38,14 +39,16 @@ public class CourseHiddenElements {
       Object hiddenRegex = entryInfo.get("byRegex");
       if (hiddenRegex instanceof String) {
         // Hide by language-independent regex
-        hiddenRegexes.add((String) hiddenRegex);
+        hiddenRegexes.add(Pattern.compile((String) hiddenRegex, Pattern.CASE_INSENSITIVE));
         continue;
       } else if (hiddenRegex instanceof JSONObject) {
         // Hide by language-dependent regex
         JSONObject hiddenRegexObject = (JSONObject)hiddenRegex;
         for (String key : hiddenRegexObject.keySet()) {
           String regex = hiddenRegexObject.getString(key);
-          hiddenLanguageSpecificRegexes.computeIfAbsent(key, k -> new ArrayList<>()).add(regex);
+
+          hiddenLanguageSpecificRegexes.computeIfAbsent(key, k -> new ArrayList<>())
+              .add(Pattern.compile(regex, Pattern.CASE_INSENSITIVE));
         }
 
         continue;
@@ -57,13 +60,26 @@ public class CourseHiddenElements {
     return new CourseHiddenElements(hiddenIDs, hiddenRegexes, hiddenLanguageSpecificRegexes);
   }
 
+  public boolean shouldHideObject(long objectId, @NotNull String objectName, @Nullable String currentLanguage) {
+    if (hiddenIDs.contains(objectId)) {
+      return true;
+    }
+
+    if (hiddenRegexes.stream().anyMatch(r -> r.matcher(objectName).find())) {
+      return true;
+    }
+
+    return currentLanguage != null && hiddenLanguageSpecificRegexes.getOrDefault(currentLanguage, new ArrayList<>())
+        .stream().anyMatch(r -> r.matcher(objectName).find());
+  }
+
   public CourseHiddenElements() {
     this(null, null, null);
   }
 
-  public CourseHiddenElements(@Nullable List<Integer> hiddenIDs,
-                              @Nullable List<String> hiddenRegexes,
-                              @Nullable Map<String, List<String>> hiddenLanguageSpecificRegexes) {
+  public CourseHiddenElements(@Nullable List<Long> hiddenIDs,
+                              @Nullable List<Pattern> hiddenRegexes,
+                              @Nullable Map<String, List<Pattern>> hiddenLanguageSpecificRegexes) {
     if (hiddenIDs != null) {
       this.hiddenIDs.addAll(hiddenIDs);
     }
