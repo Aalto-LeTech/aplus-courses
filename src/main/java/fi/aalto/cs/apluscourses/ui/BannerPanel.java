b106d1e7a5f8700f6ee7cafd9c22f1b6bf48f13e
Taikelenn <taikelenn@kasumata.ee>
2023-07-24 00:48:44 +0300

    Delay setting background colors until after the component is added

--- src/main/java/fi/aalto/cs/apluscourses/ui/BannerPanel.java
+++ src/main/java/fi/aalto/cs/apluscourses/ui/BannerPanel.java
@@ -4,6 +4,7 @@ import com.intellij.ui.JBColor;
 import com.intellij.ui.components.panels.OpaquePanel;
 import com.intellij.util.ui.JBUI;
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
@@ -14,18 +15,21 @@ public class BannerPanel extends JPanel {
   @NotNull
   private final JLabel bannerText = new JLabel();
 
+  @NotNull
+  private final JPanel containerPanel;
+
   /**
    * Constructor for the banner.
    */
   public BannerPanel() {
     super(new BorderLayout());
 
-    JPanel panel = new OpaquePanel(new BorderLayout());
-    panel.add(BorderLayout.CENTER, bannerText);
-    panel.setBorder(JBUI.Borders.empty(5, 0, 5, 5));
-    panel.setMinimumSize(new Dimension(0, 0));
+    containerPanel = new OpaquePanel(new BorderLayout());
+    containerPanel.add(BorderLayout.CENTER, bannerText);
+    containerPanel.setBorder(JBUI.Borders.empty(5, 0, 5, 5));
+    containerPanel.setMinimumSize(new Dimension(0, 0));
 
-    add(BorderLayout.CENTER, panel);
+    add(BorderLayout.CENTER, containerPanel);
 
     setBorder(BorderFactory.createCompoundBorder(
         BorderFactory.createMatteBorder(0, 0, 1, 0, JBColor.border()),
@@ -35,5 +39,10 @@ public class BannerPanel extends JPanel {
 
   public void setText(String text) {
     bannerText.setText(text);
+  }
+
+  public void setBannerColor(@NotNull Color color) {
+    containerPanel.setBackground(color);
+    setBackground(color);
   }
 }
