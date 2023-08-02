eebf482f5a8305c8ec6ea3324bfc90b8aead9b17
Taikelenn <taikelenn@kasumata.ee>
2023-08-03 00:06:58 +0300

    Try checkout someplace else

--- start_local_a+_env.sh
+++ start_local_a+_env.sh
@@ -3,11 +3,11 @@
 # preparing and compiling the A+ env components (https://apluslms.github.io/guides/quick/)
 git clone --depth 1 https://github.com/apluslms/aplus-manual.git my_new_course &&
 cd my_new_course &&
-git checkout ea39d0144dc97e5438f6c19a70085a03708ccdee &&
 wget https://github.com/mikefarah/yq/releases/download/v4.16.2/yq_linux_amd64 && chmod +x yq_linux_amd64 &&
 ./yq_linux_amd64 eval -i '.services.plus.volumes += ["../tools/aplus-init.sh:/srv/aplus-init.sh:ro", "../tools/simple_course.py:/srv/simple_course.py:ro"]' docker-compose.yml &&
 git submodule init &&
 git submodule update &&
+git checkout ea39d0144dc97e5438f6c19a70085a03708ccdee &&
 chmod +x docker-fast-compile.sh && ./docker-fast-compile.sh &&
 # modifying the initial script (just cutting off the last part of it), so it would run in the background
 head -102 docker-up.sh > docker-up-custom.sh &&
