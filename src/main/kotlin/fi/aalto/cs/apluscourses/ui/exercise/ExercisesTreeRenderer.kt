6fbf3b369bc377c1e723855a3be1ed0cce684107
Jaakko Nakaza <jaakko.nakaza@aalto.fi>
2024-04-22 12:06:19 +0300

    Initial version of new ExercisesView

---
+++ src/main/kotlin/fi/aalto/cs/apluscourses/ui/exercise/ExercisesTreeRenderer.kt
@@ -0,0 +1,322 @@
+package fi.aalto.cs.apluscourses.ui.exercise
+
+import com.intellij.ide.ui.UISettings
+import com.intellij.ide.util.treeView.NodeRenderer
+import com.intellij.ui.ColorUtil
+import com.intellij.ui.JBColor
+import com.intellij.ui.SimpleTextAttributes
+import com.intellij.ui.components.fields.ExtendableTextComponent
+import com.intellij.util.ui.JBFont
+import fi.aalto.cs.apluscourses.MyBundle.message
+import fi.aalto.cs.apluscourses.model.exercise.Exercise
+import icons.PluginIcons
+import java.awt.*
+import java.awt.geom.RoundRectangle2D
+import javax.swing.Icon
+import javax.swing.JButton
+import javax.swing.JTree
+import javax.swing.tree.DefaultMutableTreeNode
+
+
+class ExercisesTreeRenderer : NodeRenderer() {
+    private lateinit var item: ExercisesView.ExercisesTreeItem
+
+    private val actionButton: JButton = JButton("Submit").apply {
+//        isFocusable = false
+//        addMouseListener(object : MouseAdapter() {
+//            override fun mouseEntered(e: MouseEvent?) {
+//                background = JBColor.red//JBUI.CurrentTheme.ActionButton.hoverBackground()
+//            }
+//
+//            override fun mouseExited(e: MouseEvent?) {
+//                background = JBColor.background()
+//            }
+//        })
+
+    }
+
+//    private var hovering = false
+//    private var mousePosition: Point? = Point()
+
+    override fun customizeCellRenderer(
+        tree: JTree,
+        value: Any,
+        isSelected: Boolean,
+        isExpanded: Boolean,
+        isLeaf: Boolean,
+        row: Int,
+        hasFocus: Boolean
+    ) {
+//        hovering = getHoveredRow(tree) == row
+//        mousePosition = tree.mousePosition
+//        background = if (row % 2 == 0) {
+//            JBColor.red
+//        } else {
+//            JBColor.background()
+//        }
+        this.item = (value as DefaultMutableTreeNode).userObject as ExercisesView.ExercisesTreeItem
+        when (val item = value.userObject as ExercisesView.ExercisesTreeItem) {
+            is ExercisesView.NewSubmissionItem -> {
+                isEnabled = true
+                append(item.displayName(), SimpleTextAttributes.REGULAR_ITALIC_ATTRIBUTES, true)
+                icon = PluginIcons.A_PLUS_PLUS
+                toolTipText = message("ui.exercise.ExercisesTreeRenderer.submit")
+            }
+
+            is ExercisesView.ExerciseGroupItem -> {
+                val group = item.group
+                append("", SimpleTextAttributes.REGULAR_ATTRIBUTES, true) // disable search highlighting
+                append(group.name, SimpleTextAttributes.REGULAR_ATTRIBUTES, false)
+                isEnabled = true
+                icon = if (group.exercises.any { exercise -> !exercise.isDetailsLoaded }) {
+                    PluginIcons.A_PLUS_LOADING
+                } else if (group.isOpen) {
+                    PluginIcons.A_PLUS_EXERCISE_GROUP
+                } else {
+                    PluginIcons.A_PLUS_EXERCISE_GROUP_CLOSED
+                }
+                toolTipText = ""
+            }
+
+            is ExercisesView.ExerciseItem -> {
+                val exercise = item.exercise
+                append(exercise.name, SimpleTextAttributes.REGULAR_ATTRIBUTES, true)
+//                if (viewModel.statusText.trim { it <= ' ' }.isNotEmpty()) {
+//                    append(" [" + viewModel.statusText + "]", STATUS_TEXT_STYLE)
+//                }
+                isEnabled = exercise.isSubmittable
+//                toolTipText = if (exercise.isSubmittable
+//                ) PluginResourceBundle.getText("ui.exercise.ExercisesTreeRenderer.useUploadButton")
+//                else PluginResourceBundle.getText("ui.exercise.ExercisesTreeRenderer.cannotSubmit")
+                icon = ExtendableTextComponent.Extension { statusToIcon(getStatus(exercise)) }.getIcon(false)
+            }
+
+            is ExercisesView.SubmissionResultItem -> {
+                val submission = item.submission
+                isEnabled = true
+                append(item.displayName(), SimpleTextAttributes.REGULAR_ATTRIBUTES, true)
+//                append(" [" + viewModel.statusText + "]", STATUS_TEXT_STYLE, false)
+//                toolTipText = PluginResourceBundle.getAndReplaceText(
+//                    "ui.exercise.ExercisesTreeRenderer.tooltip",
+//                    submission.id.toString()
+//                )
+            }
+
+            is ExercisesView.ExercisesRootItem -> {}
+        }
+
+    }
+
+
+    override fun paintComponent(g: Graphics?) {
+        if (g == null) {
+            return
+        }
+        val g2d = g.create() as Graphics2D
+        UISettings.setupAntialiasing(g2d)
+        val clip: Shape?
+        val width = width
+        val height = height
+        if (isOpaque) {
+            // paint background for expanded row
+            g2d.color = background
+            g2d.fillRect(0, 0, width, height)
+        }
+
+        var white = true
+        var status = ""
+        var isSubmittable = true
+        val text = when (val item = this.item) {
+            is ExercisesView.ExerciseItem -> {
+//                if (hovering && item.exercise.isSubmittable) {
+//                    super.paintComponent(g2d)
+//                    val size = size
+//                    val buttonSize = actionButton.preferredSize
+//                    actionButton.setBounds(
+//                        size.width - buttonSize.width - 5,
+//                        0, buttonSize.width, height
+//                    )
+////                     transparent backround
+//                    actionButton.isOpaque = false
+//                    // check if hovering
+//                    if (mousePosition != null && mousePosition!!.x < size.width - buttonSize.width - 5
+//                    ) {
+//                        actionButton.graphics?.color = JBColor(0x000000, 0x000000)
+//                        println("dijawofijaoiwf")
+//                        actionButton.border = JBUI.Borders.customLine(JBColor(0x000000, 0x000000), 1, 0, 0, 0)
+//                    } else {
+//                        actionButton.isSelected = false
+//                        actionButton.border = JBUI.Borders.empty()
+//                    }
+//
+//                    SwingUtilities.paintComponent(g2d, actionButton, this, actionButton.bounds)
+//                    return
+//                }
+                val exercise = item.exercise
+                isSubmittable = exercise.isSubmittable
+//                if (submittable) {
+//                    g2d.
+//                }
+                white = false
+                val userPoints = exercise.userPoints
+                val maxPoints = exercise.maxPoints
+                g2d.font = JBFont.regular()
+                val statusEnum = getStatus(exercise)
+                g2d.color = when (statusEnum) {
+                    Status.FULL_POINTS ->
+                        JBColor(0x8bc34a, 0x8bc34a)
+
+                    Status.NO_POINTS, Status.PARTIAL_POINTS ->
+                        JBColor(0xffb74d, 0xffb74d)
+
+                    else -> JBColor(0xc5c5c5, 0xc5c5c5)
+                }
+                if (!isSubmittable) {
+                    g2d.color = ColorUtil.withAlpha(g2d.color, 0.5)
+                }
+                val statusText: String =
+                    if (statusEnum == Status.OPTIONAL_PRACTICE) {
+                        exercise.difficulty
+                    } else {
+                        val lateString = if (exercise.isLate()) "late, " else ""
+                        "${lateString}${exercise.submissionResults.size} of ${exercise.maxSubmissions} ${exercise.difficulty}"
+                    }
+                status = statusText
+                "${userPoints}/${maxPoints}"
+            }
+
+            is ExercisesView.ExerciseGroupItem -> {
+                val group = item.group
+                if (group.maxPoints == 0) {
+                    super.paintComponent(g)
+                    return
+                }
+                g2d.font = JBFont.regular().deriveFont(Font.BOLD)
+                g2d.color = JBColor(0x005EB8, 0x005EB8)
+                "${group.userPoints}/${group.maxPoints}"
+            }
+
+            is ExercisesView.SubmissionResultItem -> {
+                val submission = item.submission
+                g2d.font = JBFont.regular()
+                g2d.color = if (submission.userPoints == submission.maxPoints) {
+                    JBColor(0x8bc34a, 0x8bc34a)
+                } else {
+                    JBColor(0xffb74d, 0xffb74d)
+                }
+                "${submission.userPoints}/${submission.maxPoints}"
+            }
+
+            else -> {
+                super.paintComponent(g)
+                return
+            }
+
+        }
+
+
+        val fontMetrics: FontMetrics = g2d.fontMetrics
+        val stringBounds = fontMetrics.getStringBounds(text, g2d).bounds
+        val textX = (width - stringBounds.width) - 10
+        val textY =
+            getTextBaseLine(g2d.fontMetrics, height) // height - stringBounds.height) / 2 + fontMetrics.ascent + 1
+
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+        val roundRectangle =
+            RoundRectangle2D.Double(
+                textX - 15.0,
+                textY - stringBounds.height + 2.0,
+                stringBounds.width.toDouble() + 10.0,
+                stringBounds.height.toDouble() + 2.0,
+                stringBounds.height.toDouble(),
+                stringBounds.height.toDouble()
+            )
+        when (item) {
+            is ExercisesView.SubmissionResultItem -> {
+                g2d.draw(roundRectangle)
+            }
+
+            else -> {
+                g2d.fill(roundRectangle)
+                g2d.color =
+                    if (white)
+                        JBColor(0xFFFFFF, 0xFFFFFF)
+                    else
+                        JBColor(0x000000, 0x000000)
+            }
+        }
+
+        if (!isSubmittable) {
+            g2d.color = ColorUtil.withAlpha(g2d.color, 0.8)
+        }
+        g2d.drawString(text, textX - 10, textY)
+
+        if (status.isNotEmpty()) {
+            val statusBounds = fontMetrics.getStringBounds(status, g2d).bounds
+            val statusX = (width - statusBounds.width) - (stringBounds.width + 18)
+            g2d.color = JBColor.gray
+            if (!isSubmittable) {
+                g2d.color = ColorUtil.withAlpha(g2d.color, 0.5)
+            }
+            g2d.drawString(status, statusX - 10, textY)
+        }
+
+
+        clip = g2d.clip
+        g2d.clipRect(0, 0, width, height)
+
+        super.paintComponent(g2d)
+
+        // restore clip area if needed
+        if (clip != null) g2d.clip = clip
+    }
+
+    companion object {
+        private fun getStatus(exercise: Exercise): Status {
+            return if (exercise.isInGrading()) {
+                Status.IN_GRADING
+            } else if (!exercise.isDetailsLoaded) {
+                Status.LOADING
+            } else if (exercise.isOptional) {
+                Status.OPTIONAL_PRACTICE
+            } else if (exercise.submissionResults.isEmpty()) {
+                Status.NO_SUBMISSIONS
+            } else if (exercise.userPoints == exercise.maxPoints) {
+                Status.FULL_POINTS
+            } else if (exercise.isLate()) {
+                Status.LATE
+            } else if (exercise.userPoints == 0) {
+                Status.NO_POINTS
+            } else {
+                Status.PARTIAL_POINTS
+            }
+        }
+
+        private fun statusToIcon(exerciseStatus: Status): Icon {
+            return when (exerciseStatus) {
+                Status.OPTIONAL_PRACTICE -> PluginIcons.A_PLUS_OPTIONAL_PRACTICE
+                Status.NO_SUBMISSIONS -> PluginIcons.A_PLUS_NO_SUBMISSIONS
+                Status.NO_POINTS -> PluginIcons.A_PLUS_NO_POINTS
+                Status.PARTIAL_POINTS -> PluginIcons.A_PLUS_PARTIAL_POINTS
+                Status.FULL_POINTS -> PluginIcons.A_PLUS_FULL_POINTS
+                Status.LATE -> PluginIcons.A_PLUS_LATE
+                Status.IN_GRADING, Status.LOADING -> PluginIcons.A_PLUS_LOADING
+            }
+        }
+
+        private val STATUS_TEXT_STYLE = SimpleTextAttributes(
+            SimpleTextAttributes.STYLE_ITALIC or SimpleTextAttributes.STYLE_SMALLER, null
+        )
+
+        enum class Status {
+            OPTIONAL_PRACTICE,
+            NO_SUBMISSIONS,
+            NO_POINTS,
+            PARTIAL_POINTS,
+            FULL_POINTS,
+            IN_GRADING,
+            LATE,
+            LOADING
+        }
+    }
+}
---
+++ src/main/kotlin/fi/aalto/cs/apluscourses/ui/exercise/ExercisesView.kt
@@ -0,0 +1,261 @@
+package fi.aalto.cs.apluscourses.ui.exercise
+
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.SimpleToolWindowPanel
+import com.intellij.ui.*
+import com.intellij.ui.components.TextComponentEmptyText
+import com.intellij.ui.hover.TreeHoverListener
+import com.intellij.ui.scale.JBUIScale.scale
+import com.intellij.ui.treeStructure.SimpleTree
+import com.intellij.util.ui.JBUI
+import com.intellij.util.ui.UIUtil
+import com.intellij.util.ui.tree.TreeUtil
+import fi.aalto.cs.apluscourses.model.exercise.Exercise
+import fi.aalto.cs.apluscourses.model.exercise.ExerciseGroup
+import fi.aalto.cs.apluscourses.model.exercise.SubmissionResult
+import fi.aalto.cs.apluscourses.services.exercise.ExercisesTreeFilterService
+import fi.aalto.cs.apluscourses.services.exercise.ExercisesUpdaterService
+import fi.aalto.cs.apluscourses.services.exercise.SelectedExerciseService
+import java.awt.BorderLayout
+import java.awt.Color
+import javax.swing.ScrollPaneConstants
+import javax.swing.tree.DefaultMutableTreeNode
+import javax.swing.tree.TreeSelectionModel
+
+
+class ExercisesView(project: Project) :
+    SimpleToolWindowPanel(true, true) {
+
+    val exerciseGroupsFilteringTree: ExercisesTreeView = ExercisesTreeView(project).apply {
+        installSearchField()
+    }
+
+    val searchTextField: SearchTextField
+
+    /**
+     * Creates an ExerciseView that uses mainViewModel to dynamically adjust its UI components.
+     */
+    init {
+        exerciseGroupsFilteringTree.tree.selectionModel.selectionMode =
+            TreeSelectionModel.SINGLE_TREE_SELECTION
+        TreeUtil.selectFirstNode(exerciseGroupsFilteringTree.tree)
+
+        val treeComponent = exerciseGroupsFilteringTree.component
+        val scrollPane = ScrollPaneFactory.createScrollPane(treeComponent, true)
+        scrollPane.horizontalScrollBarPolicy = ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
+        scrollPane.verticalScrollBarPolicy = ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS
+
+        exerciseGroupsFilteringTree.updateTree()
+        searchTextField = exerciseGroupsFilteringTree.installSearchField()
+
+        val panel = JBUI.Panels.simplePanel()
+//        panel.add(searchTextField, BorderLayout.NORTH)
+        panel.add(scrollPane, BorderLayout.CENTER)
+
+        panel.focusTraversalPolicy = ListFocusTraversalPolicy(
+            listOf(searchTextField, scrollPane)
+        )
+        panel.isFocusTraversalPolicyProvider = true
+        panel.isFocusCycleRoot = true
+        setContent(panel)
+    }
+
+
+    fun updateTree() {
+//        val scroll = scrollPane.verticalScrollBar.value
+//        val treeState = TreeState.createOn(exerciseGroupsFilteringTree.tree, exerciseGroupsFilteringTree.root)
+//        invokeLater {
+        exerciseGroupsFilteringTree.updateTree()
+//            panel.remove(searchTextField)
+//            searchTextField.removeNotify()
+//            searchTextField = exerciseGroupsFilteringTree.installSearchField()
+//            panel.add(searchTextField, BorderLayout.NORTH)
+//            panel.revalidate()
+//            panel.repaint()
+//        }
+
+//        treeState.applyTo(exerciseGroupsFilteringTree.tree)
+//        scrollPane.verticalScrollBar.value = scroll
+    }
+
+    fun updateExercise(exercise: Exercise) {
+        return
+        TreeUtil.findNode(exerciseGroupsFilteringTree.root) { node ->
+            when (node.userObject) {
+                is ExerciseItem -> (node.userObject as ExerciseItem).exercise.id == exercise.id
+                else -> false
+            }
+        }?.let {
+            val path = TreeUtil.getPath(exerciseGroupsFilteringTree.root, it)
+            TreeUtil.repaintPath(exerciseGroupsFilteringTree.tree, path)
+//            exerciseGroupsFilteringTree.tree.repaint()
+        }
+    }
+
+
+    sealed interface ExercisesTreeItem {
+        fun displayName(): String
+
+        fun children(): List<ExercisesTreeItem>
+    }
+
+    data class ExercisesRootItem(val project: Project) : ExercisesTreeItem {
+        override fun displayName(): String = ""
+
+        override fun children(): List<ExerciseGroupItem> =
+            project.service<ExercisesUpdaterService>().state.exerciseGroups.map { group ->
+                ExerciseGroupItem(
+                    group,
+                    group.exercises.map { exercise ->
+                        val newSubmission =
+                            if (exercise.isSubmittable) listOf(NewSubmissionItem(exercise)) else emptyList()
+                        ExerciseItem(
+                            exercise,
+                            (exercise.submissionResults.mapIndexed { i, submission ->
+                                SubmissionResultItem(submission, i, exercise)
+                            } + newSubmission
+                                    ).reversed() // Reverse order of submissions to show the latest first
+                        )
+                    })
+            }.filterNot( // Filter groups
+                ApplicationManager.getApplication()
+                    .service<ExercisesTreeFilterService>().state.exercisesGroupFilter()
+            ).filter { group -> // Filter out empty groups
+                group.children().isNotEmpty()
+            }
+    }
+
+    data class ExerciseGroupItem(val group: ExerciseGroup, private val children: List<ExerciseItem>) :
+        ExercisesTreeItem {
+        override fun displayName(): String = group.name
+
+        override fun children(): List<ExerciseItem> = children.filterNot( // Filter exercises
+            ApplicationManager.getApplication()
+                .service<ExercisesTreeFilterService>().state.exercisesFilter()
+        )
+    }
+
+    data class ExerciseItem(val exercise: Exercise, private val children: List<ExercisesTreeItem>) :
+        ExercisesTreeItem {
+        override fun displayName(): String = exercise.name
+
+        override fun children(): List<ExercisesTreeItem> = children
+    }
+
+    data class SubmissionResultItem(
+        val submission: SubmissionResult,
+        val index: Int,
+        val exercise: Exercise
+    ) :
+        ExercisesTreeItem {
+        override fun displayName(): String = "Submission ${index + 1}"
+
+        override fun children(): List<ExercisesTreeItem> = emptyList()
+    }
+
+    data class NewSubmissionItem(val exercise: Exercise) : ExercisesTreeItem {
+        override fun displayName(): String = "New submission"
+
+        override fun children(): List<ExercisesTreeItem> = emptyList()
+    }
+
+    class SimpleExercisesTree : SimpleTree() {
+        override fun isFileColorsEnabled(): Boolean = true
+        override fun getFileColorForRow(row: Int): Color? =
+            if (getPathForRow(row) == null) null
+            else if (row % 2 == 0) UIUtil.getTableBackground()
+            else if (JBColor.isBright()) UIUtil.getTableBackground().brighter()
+            else ColorUtil.darker(UIUtil.getTableBackground(), 1)
+    }
+
+    class ExercisesTreeView(project: Project) :
+        FilteringTree<DefaultMutableTreeNode, ExercisesTreeItem>(
+            SimpleExercisesTree(),
+            DefaultMutableTreeNode(ExercisesRootItem(project))
+        ) {
+        init {
+            tree.addTreeSelectionListener {
+                if (tree.lastSelectedPathComponent == null) return@addTreeSelectionListener
+                val selectedNode = tree.lastSelectedPathComponent as DefaultMutableTreeNode
+                val selected = selectedNode.userObject as? ExercisesTreeItem ?: return@addTreeSelectionListener
+
+                val selectedExercise = when (selected) {
+                    is ExerciseItem -> selected.exercise
+                    is SubmissionResultItem -> selected.exercise
+                    is NewSubmissionItem -> selected.exercise
+                    else -> null
+                }
+
+                project.service<SelectedExerciseService>().selectedExercise = selectedExercise
+            }
+            tree.isRootVisible = false
+            tree.cellRenderer = ExercisesTreeRenderer()
+            tree.rowHeight = scale(24)
+            tree.toggleClickCount = 1
+            tree.putClientProperty(AnimatedIcon.ANIMATION_IN_RENDERER_ALLOWED, true)
+            SmartExpander.installOn(tree)
+            TreeHoverListener.DEFAULT.addTo(tree)
+        }
+
+        fun updateTree() {
+            searchModel.updateStructure()
+            tree.revalidate()
+            tree.repaint()
+        }
+
+        override fun useIdentityHashing(): Boolean {
+            return false
+        }
+
+        override fun getNodeClass() = DefaultMutableTreeNode::class.java
+
+        override fun getChildren(item: ExercisesTreeItem): List<ExercisesTreeItem> =
+            item.children()
+
+        override fun getText(item: ExercisesTreeItem?): String = when (item) {
+            is ExerciseItem -> item.exercise.name
+            is SubmissionResultItem -> item.exercise.name
+            is NewSubmissionItem -> item.exercise.name
+            else -> ""
+        }
+
+        override fun createNode(item: ExercisesTreeItem): DefaultMutableTreeNode =
+            DefaultMutableTreeNode(item)
+
+        override fun installSearchField(): SearchTextField {
+            return super.installSearchField().apply {
+                textEditor.apply {
+                    emptyText.text = "Search"
+                    accessibleContext.accessibleName = "Search"
+                    TextComponentEmptyText.setupPlaceholderVisibility(this)
+
+//                    addKeyboardAction(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0)) { activateItems(tree) }
+                }
+            }
+        }
+
+    }
+
+
+}
+
+
+/**
+ * Sets the nodeAppliedListener as OpenExerciseItemAction if the course isn't supported in ShowFeedbackAction,
+ * else ShowFeedbackAction.
+ */
+//    fun setSubmissionAction(feedbackEnabled: Boolean) {
+//        if (!feedbackEnabled) {
+//            exerciseGroupsTree.addNodeAppliedListener(
+//                SubmissionResultViewModel::class.java,
+//                ActionUtil.createOnEventLauncher(OpenExerciseItemAction.ACTION_ID, exerciseGroupsTree)
+//            )
+//        } else {
+//            exerciseGroupsTree.addNodeAppliedListener(
+//                SubmissionResultViewModel::class.java,
+//                ActionUtil.createOnEventLauncher(ShowFeedbackAction.ACTION_ID, exerciseGroupsTree)
+//            )
+//        }
+//    }
