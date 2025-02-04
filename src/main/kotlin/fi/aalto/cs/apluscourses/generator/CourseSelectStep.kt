93e45d578f7975707a09f7da68a97f77fa85be2f
Jaakko Nakaza <jaakko.nakaza@aalto.fi>
2025-02-04 15:47:56 +0200

    Refactor project creation UI

--- src/main/kotlin/fi/aalto/cs/apluscourses/generator/CourseSelectStep.kt
+++ src/main/kotlin/fi/aalto/cs/apluscourses/generator/CourseSelectStep.kt
@@ -3,11 +3,15 @@ package fi.aalto.cs.apluscourses.generator
 import com.intellij.ide.util.projectWizard.ModuleWizardStep
 import com.intellij.openapi.components.service
 import com.intellij.openapi.observable.properties.AtomicProperty
-import com.intellij.ui.AnimatedIcon
+import com.intellij.openapi.observable.util.and
+import com.intellij.openapi.observable.util.isNull
+import com.intellij.openapi.observable.util.notEqualsTo
+import com.intellij.openapi.options.ConfigurationException
+import com.intellij.openapi.ui.ValidationInfo
 import com.intellij.ui.ColoredListCellRenderer
 import com.intellij.ui.SimpleTextAttributes
-import com.intellij.ui.components.JBList
 import com.intellij.ui.dsl.builder.AlignX
+import com.intellij.ui.dsl.builder.bindItem
 import com.intellij.ui.dsl.builder.bindText
 import com.intellij.ui.dsl.builder.panel
 import com.intellij.ui.dsl.gridLayout.UnscaledGaps
@@ -15,41 +19,35 @@ import com.intellij.util.application
 import com.intellij.util.ui.JBUI
 import fi.aalto.cs.apluscourses.MyBundle.message
 import fi.aalto.cs.apluscourses.icons.CoursesIcons
+import fi.aalto.cs.apluscourses.services.PluginSettings
 import fi.aalto.cs.apluscourses.services.course.CoursesFetcher
+import fi.aalto.cs.apluscourses.ui.Utils.bindItem
+import fi.aalto.cs.apluscourses.ui.Utils.list
 import javax.swing.JComponent
 import javax.swing.JList
-import javax.swing.ListSelectionModel
-import javax.swing.event.ListSelectionListener
 
 class CourseSelectStep(private val config: APlusModuleConfig) : ModuleWizardStep() {
-    private val courseList = JBList<CoursesFetcher.CourseConfig>()
+    private val courses = AtomicProperty<List<CoursesFetcher.CourseConfig>>(emptyList())
+
     private val courseConfigUrl = AtomicProperty("")
-    private val errorMessage = AtomicProperty("")
+    private val course = AtomicProperty<CoursesFetcher.CourseConfig?>(null)
+    private val language = AtomicProperty("")
 
     init {
-        courseList.cellRenderer = object : ColoredListCellRenderer<CoursesFetcher.CourseConfig>() {
-            override fun customizeCellRenderer(
-                list: JList<out CoursesFetcher.CourseConfig>,
-                item: CoursesFetcher.CourseConfig,
-                index: Int,
-                selected: Boolean,
-                hasFocus: Boolean
-            ) {
-                icon = CoursesIcons.ExerciseGroup
-                append(item.name, SimpleTextAttributes(SimpleTextAttributes.STYLE_BOLD, null))
-                append(" " + item.semester, SimpleTextAttributes(SimpleTextAttributes.STYLE_ITALIC, null))
+        course.afterChange {
+            if (it != null) {
+                courseConfigUrl.set(it.url)
+                language.set(it.language ?: "")
             }
         }
-        courseList.putClientProperty(AnimatedIcon.ANIMATION_IN_RENDERER_ALLOWED, true)
-        courseList.selectionMode = ListSelectionModel.SINGLE_SELECTION
-
-        courseList.addListSelectionListener(ListSelectionListener { e ->
-            val url = courseList.selectedValue?.url ?: return@ListSelectionListener
-            courseConfigUrl.set(url)
-        })
+        courseConfigUrl.afterChange {
+            if (course.get()?.url != it) {
+                course.set(null)
+            }
+        }
 
         application.service<CoursesFetcher>()
-            .fetchCourses { courses -> courseList.setListData(courses.toTypedArray()) }
+            .fetchCourses { courses.set(it) }
     }
 
     override fun getComponent(): JComponent {
@@ -64,7 +62,29 @@ class CourseSelectStep(private val config: APlusModuleConfig) : ModuleWizardStep
                     text(message("generator.APlusModuleBuilder.selectCourse"))
                 }
                 row {
-                    cell(courseList).resizableColumn().align(AlignX.FILL)
+                    list(courses, object : ColoredListCellRenderer<CoursesFetcher.CourseConfig>() {
+                        override fun customizeCellRenderer(
+                            list: JList<out CoursesFetcher.CourseConfig>,
+                            item: CoursesFetcher.CourseConfig?,
+                            index: Int,
+                            selected: Boolean,
+                            hasFocus: Boolean
+                        ) {
+                            icon = CoursesIcons.ExerciseGroup
+                            append(item?.name ?: "", SimpleTextAttributes(SimpleTextAttributes.STYLE_BOLD, null))
+                            append(
+                                (" " + item?.semester),
+                                SimpleTextAttributes(SimpleTextAttributes.STYLE_ITALIC, null)
+                            )
+                        }
+                    }).resizableColumn().align(AlignX.FILL)
+                        .bindItem(course)
+                        .validationOnApply {
+                            if (course.get() == null) {
+                                return@validationOnApply ValidationInfo(message("ui.ExportModuleDialog.error.noModule"))
+                            }
+                            return@validationOnApply null
+                        }
                 }
                 row(message("generator.APlusModuleBuilder.configUrl")) {
                     textField()
@@ -72,11 +92,17 @@ class CourseSelectStep(private val config: APlusModuleConfig) : ModuleWizardStep
                         .resizableColumn()
                         .align(AlignX.FILL)
                 }
-                row {
-                    text("").bindText(errorMessage).applyToComponent {
-                        foreground = JBUI.CurrentTheme.NotificationError.borderColor()
-                    }
-                }
+                row(message("generator.APlusModuleBuilder.language")) {
+                    comboBox(PluginSettings.SUPPORTED_LANGUAGES)
+                        .bindItem(language)
+                }.visibleIf(
+                    course
+                        .isNull()
+                        .and(
+                            courseConfigUrl
+                                .notEqualsTo("")
+                        )
+                )
             }.customize(UnscaledGaps(20, 20, 20, 20))
         }
     }
@@ -86,18 +112,22 @@ class CourseSelectStep(private val config: APlusModuleConfig) : ModuleWizardStep
     override fun validate(): Boolean {
         val url = courseConfigUrl.get()
         if (url.isEmpty()) {
-            errorMessage.set(message("generator.APlusModuleBuilder.selectCourse.selectError"))
-            return false
+            throw ConfigurationException(
+                message("generator.APlusModuleBuilder.selectCourse.selectError"),
+                message("generator.APlusModuleBuilder.selectCourse.selectErrorTitle")
+            )
         }
+
         val courseConfig = application.service<CoursesFetcher>().fetchCourse(url)
         if (courseConfig == null) {
-            errorMessage.set(message("generator.APlusModuleBuilder.selectCourse.urlError"))
-            return false
+            throw ConfigurationException(
+                message("generator.APlusModuleBuilder.selectCourse.urlError"),
+                message("generator.APlusModuleBuilder.selectCourse.urlErrorTitle")
+            )
         }
-        errorMessage.set("")
         config.courseConfig = courseConfig
         config.courseConfigUrl = url
-        config.programmingLanguage = courseList.selectedValue.language ?: ""
+        config.programmingLanguage = language.get()
         return true
     }
 }
